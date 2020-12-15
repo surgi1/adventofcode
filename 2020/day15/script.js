48729620972981d73322c8c4f9158a6423d08fcd
Jan Surovec <gh1@cloudia.cz>
2020-12-15 11:57:04 +0100

    readability change for day15, added time estimate

--- 2020/day15/script.js
+++ 2020/day15/script.js
@@ -1,19 +1,18 @@
+// p2 is pretty slow, around 7 minutes
+
 var data = [2,20,0,4,1,17];
 //var stopNr = 2020; // p1
 var stopNr = 30000000; // p2
 
 var history = [];
 var len = data.length;
-var last;
 
-for (var i = 0; i < stopNr; i++) {
-    if (i % 100000 == 0) console.log('Mticks', i/1000000);
-    if (i < len) {
-        last = data[i];
-        history[last] = [i];
-        continue;
-    }
+data.map((d,i) => history[d] = [i]);
 
+var last = data.pop();
+
+console.time('runtime');
+for (var i = len; i < stopNr; i++) {
     var num = 0;
     if (history[last].length > 1) num = history[last][1]-history[last].shift();
 
@@ -22,5 +21,6 @@ for (var i = 0; i < stopNr; i++) {
 
     last = num;
 }
+console.timeEnd('runtime');
 
 console.log(last);
