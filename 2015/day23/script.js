7bc77da9835bdbe8f491cfc83c3663504bd1eac9
Jan Surovec <gh1@cloudia.cz>
2020-12-17 11:49:02 +0100

    added day23 of aoc 2015

---
+++ 2015/day23/script.js
@@ -0,0 +1,37 @@
+var regs = {a:1,b:0}, pointer = 0, program = [], ticks = 0;
+
+function readInput() {
+    input.map(line => {
+        var arr = line.split(' ');
+        var pars = arr[1].split(',');
+        pars.map((p,i) => {
+            if (!isNaN(p)) pars[i] = parseInt(p);
+        })
+        program.push({
+            instruction: arr[0],
+            pars: pars
+        })
+    })
+}
+
+function runInstruction(line) {
+    switch (line.instruction) {
+        case 'hlf': regs[line.pars[0]] = regs[line.pars[0]] >> 1; pointer++; break;
+        case 'tpl': regs[line.pars[0]] *= 3; pointer++; break;
+        case 'inc': regs[line.pars[0]]++;pointer++; break;
+        case 'jmp': pointer += line.pars[0]; break;
+        case 'jie': if (regs[line.pars[0]] % 2 == 0) pointer += line.pars[1]; else pointer++; break;
+        case 'jio': if (regs[line.pars[0]] == 1) pointer += line.pars[1]; else pointer++; break;
+    }
+}
+
+function run() {
+    while(program[pointer]) {
+        runInstruction(program[pointer]);
+        ticks++;
+    }
+    console.log('ticks', ticks, 'regs', regs);
+}
+
+readInput();
+run();
