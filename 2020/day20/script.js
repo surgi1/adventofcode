c8925b8ca7d3e05cd1afcbc3553aaba2a91dca80
Jan Surovec <surgi1@gmail.com>
2020-12-20 14:43:44 +0100

    another half an hour in on day 20 aoc 2020, part 1 done, part 2 wip

--- 2020/day20/script.js
+++ 2020/day20/script.js
@@ -51,12 +51,60 @@ function readInput() {
     })
 }
 
-function checkFit(tile) {
-    tile.configurations.map((cfg, cfgId) => {
-        console.log('checking tile', tile.id, 'cfg index', cfgId);
-        // TODO
-        // lets try to find this tile a perfect fit, 4 another tiles that in some config fit it
+function getTileById(id) {
+    var found = false;
+    tiles.some(t => {
+        if (t.id == id) {
+            found = t;
+            return true;
+        }
     })
+    return found;
+}
+
+function matchFootprints(fp1, fp2) {
+    var found = false;
+    var matchId1 = false, matchId2 = false;
+    //console.log('match fp', fp1, fp2);
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (fp1[i] == fp2[j]) {
+                found = true;
+                matchId1 = i; matchId2 = j;
+                break;
+            }
+        }
+        if (found) break;
+    }
+    return found;
+}
+
+function checkTileCfgFit(tile, cfgId) {
+    var cfg = tile.configurations[cfgId];
+    var adjacentTilesIds = [];
+    tiles.filter(t => t.id != tile.id).map(t => {
+        t.configurations.map((tCfg, tCfgId) => {
+            //console.log('t', tCfg, tCfgId);console.log('tile', tCfg, tCfgId);
+            if (matchFootprints(cfg.footprints, tCfg.footprints)) {
+                //console.log('tile id', tile.id, 'its respective configuration id', cfgId, 'can be placed next to tile id', t.id, 'in its configurations id', tCfgId);
+                if (!adjacentTilesIds.includes(t.id)) adjacentTilesIds.push(t.id);
+            }
+        })
+    })
+    return adjacentTilesIds;
+}
+
+function checkTileFit(tile) {
+    var found = 0; var adjacentTilesIds = [];
+    tile.configurations.map((cfg, cfgId) => {
+        //console.log('checking tile', tile.id, 'cfg index', cfgId);
+        var cfgsAdjacentTilesIds = checkTileCfgFit(tile, cfgId);
+        cfgsAdjacentTilesIds.map(c => {
+            if (!adjacentTilesIds.includes(c)) adjacentTilesIds.push(c);
+        })
+        // todo switch matched tiles to found states and move on
+    })
+    return adjacentTilesIds;
 }
 
 // once we have all the configurations for all the tiles, we need to map those numbers together
@@ -64,3 +112,13 @@ function checkFit(tile) {
 
 readInput();
 console.log('tiles', tiles);
+
+var cornerOnes = [];
+
+tiles.map(tile => {
+    var fits = checkTileFit(tile);
+    tile.positionInImage = fits.length; // 2 = corner, 3 = side, 4 = inner
+    tile.adjacentTilesIds = fits;
+})
+
+console.log('corner ones', tiles.filter(t => t.positionInImage == 2)); // p1
