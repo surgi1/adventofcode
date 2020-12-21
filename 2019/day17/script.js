5e7e4ac9bb9621e76ebeca396e3dada868ae34e9
Jan Surovec <surgi1@gmail.com>
2020-12-21 23:34:23 +0100

    day 17 of aoc 2019, part 2

--- 2019/day17/script.js
+++ 2019/day17/script.js
@@ -5,11 +5,28 @@ var root = $('#root');
 pre = $('<pre>');
 root.append(pre);
 
+input[0] = 2;
+
+function str2ascii(s) {
+    var res = [];
+    for (var i = 0; i < s.length; i++) {
+        res.push(s.charCodeAt(i));
+    }
+    res.push(10);
+    return res.join(',');
+}
+
+function tick(pars) {
+    if (!pars) pars = [];
+    var res = comp.run(pars);
+
+    var s = '';
+    res.output.map(code => s += String.fromCharCode(code));
+    pre.html(s);
+
+    console.log('result', res);
+}
+
 comp.load(input);
-var res = comp.run();
 
-var s = '';
-res.output.map(code => s += String.fromCharCode(code));
-pre.append(s);
-
-console.log('result', res);
+tick();
