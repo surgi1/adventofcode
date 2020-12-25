a0b812c9d7e910647cbb3bbc34db27a383ce7623
Jan Surovec <gh1@cloudia.cz>
2020-12-25 09:22:56 +0100

    added day 25 of aoc 2020

---
+++ 2020/day25/script.js
@@ -0,0 +1,33 @@
+var input = [11404017, 13768789], initialSN = 7;
+var loopSizes = [];
+
+function transform(num, initSN) {
+    return (num*initSN) % 20201227;
+}
+
+function getKey(num, loopSize) {
+    var n = num;
+    for (var i = 2; i <= loopSize; i++) {
+        n = transform(n, num);
+    }
+    return n;
+}
+
+var loopSize = 2, found = 0, n = initialSN;
+
+while (found < 2) {
+
+    n = transform(n, initialSN);
+    if (input.includes(n)) {
+        console.log('found', n, 'loopsize', loopSize);
+        loopSizes[input.indexOf(n)] = loopSize;
+        found++;
+    }
+
+    loopSize++;
+    if (loopSize % 1000000 == 0) console.log('Mtransforms so far', loopSize/1000000);
+
+}
+
+console.log('enc key 1', getKey(input[0], loopSizes[1]));
+console.log('enc key 2', getKey(input[1], loopSizes[0]));
