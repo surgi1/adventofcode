eb439fe32d463b15bdf965dcd8af14d7c68ccbf8
Jan Surovec <gh1@cloudia.cz>
2021-01-04 11:07:34 +0100

    added days 18 and 19 of aoc 2016

---
+++ 2016/day18/script.js
@@ -0,0 +1,28 @@
+const init = '.^..^....^....^^.^^.^.^^.^.....^.^..^...^^^^^^.^^^^.^.^^^^^^^.^^^^^..^.^^^.^^..^.^^.^....^.^...^^.^.';
+
+const getRow = lastRow => {
+    let row = '', len = lastRow.length;
+    for (let x = 0; x < len; x++) {
+        let pattern = (x > 0 ? lastRow[x-1] : '.') + lastRow[x] + (x < len-1 ? lastRow[x+1] : '.');
+        row += ['.^^', '^^.', '^..', '..^'].includes(pattern) ? '^' : '.';
+    }
+    return row;
+}
+
+const countRow = row => {
+    let count = 0, len = row.length;
+    for (let x = 0; x < len; x++) if (row[x] == '.') count++;
+    return count;
+}
+
+const countSafeTiles = rows => {
+    let lastRow = init, count = countRow(lastRow);
+    for (let y = 1; y < rows; y++) {
+        lastRow = getRow(lastRow, y);
+        count += countRow(lastRow);
+    }
+    return count;
+}
+
+console.log(countSafeTiles(40));
+console.log(countSafeTiles(400000));
