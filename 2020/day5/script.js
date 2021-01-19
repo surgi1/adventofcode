a74c30fd6a8b69847ce53badcdc2d3120c622ce9
Jan Surovec <gh1@cloudia.cz>
2021-01-19 11:42:48 +0100

    revisited part 2 of day 5

--- 2020/day5/script.js
+++ 2020/day5/script.js
@@ -1,8 +1,5 @@
-console.log('part 1', Math.max(...data));
+let min = Math.min(...data),
+    max = Math.max(...data);
 
-let root = $('#root');
-data.map(d => {
-    let row = d >> 3, col = d & 0b0000000111;
-    let el = $('<div class="seat" style="width:50px;height:15px;background-color:green;position:absolute;top:'+row*15+'px;left:'+col*50+'px;">').html(d);
-    root.append(el);
-})
+console.log('part 1', max);
+console.log('part 2', data.filter(a => a > min && a < max && !data.includes(a+1))[0]+1);
