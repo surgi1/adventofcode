5546f527c5ad53358dbe22ae4610b9b602cc9c35
Jan Surovec <gh1@cloudia.cz>
2021-01-20 18:11:03 +0100

    revisited to figure out the speed issues

--- 2020/day15/script.js
+++ 2020/day15/script.js
@@ -1,26 +1,24 @@
-// p2 is pretty slow, around 7 minutes
+let input = [2,20,0,4,1,17];
 
-let data = [2,20,0,4,1,17];
-//let stopNr = 2020; // p1
-let stopNr = 30000000; // p2
+const run = stopNr => {
+    let data = input.slice(), len = data.length;
+    let history = Array(stopNr); // initializing the array with fixed size rather history=[] cuts down the execution time dramatically, from minutes to seconds for part 2
 
-let history = [];
-let len = data.length;
+    data.map((d,i) => history[d] = [i]);
 
-data.map((d,i) => history[d] = [i]);
+    let last = data.pop();
+    for (let i = len; i < stopNr; i++) {
+        let num = 0;
+        if (history[last].length > 1) num = history[last][1]-history[last].shift();
 
-let last = data.pop();
+        if (!history[num]) history[num] = [];
+        history[num].push(i);
 
-console.time('runtime');
-for (let i = len; i < stopNr; i++) {
-    let num = 0;
-    if (history[last].length > 1) num = history[last][1]-history[last].shift();
+        last = num;
+    }
 
-    if (!history[num]) history[num] = [];
-    history[num].push(i);
-
-    last = num;
+    return last;
 }
-console.timeEnd('runtime');
 
-console.log(last);
+console.log('part 1', run(2020));
+console.log('part 2', run(30000000));
