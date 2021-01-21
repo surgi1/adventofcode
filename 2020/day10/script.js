a8341874ce275881726121349a2299eb07d4f477
Jan Surovec <gh1@cloudia.cz>
2021-01-21 14:47:45 +0100

    readability changes, comment typo fix

--- 2020/day10/script.js
+++ 2020/day10/script.js
@@ -1,33 +1,21 @@
-let chain = [];
-
-while (chain.length < input.length) {
-    let last = -1;
-    let min = 5000;
-    if (chain.length > 0) {
-        last = chain[chain.length-1];
+const constructChain = input => {
+    let chain = [];
+    while (chain.length < input.length) {
+        let last = chain.length > 0 ? chain[chain.length-1] : -1;
+        chain.push(Math.min(...input.filter(n => n > last)))
     }
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] > last) {
-            if (input[i] < min) min = input[i];
-        }
-    }
-    chain.push(min);
+    return chain;
 }
 
-const countDifferences = (chain) => {
-    let dif = [0,0,0,0];
-    for (let i = 1; i < chain.length; i++) {
-        dif[chain[i]-chain[i-1]]++;
-    }
-    return dif;
+const part1 = chain => {
+    let diff = [0,0,0,0];
+    for (let i = 1; i < chain.length; i++) diff[chain[i]-chain[i-1]]++;
+    return diff[1]*diff[3];
 }
-
-let diff = countDifferences(chain);
-console.log('part 1', diff[1]*diff[3]);
 
 // p2
 // let's apply some basic combinatorics
-// 3+ consecutive number chains with distance 1 are important, say there chain of length n
+// 3+ consecutive number chains with distance 1 are important, say theres a chain of length n
 // how many singles can be tossed out = n-2
 // how many arbitrary pairs can be tossed out = komb(n,2) = n!/((n-2)!*(2!)
 // this builds a multiplier
@@ -42,18 +30,21 @@ const combNr = (top, bottom) => fact(top)/(fact(top-bottom)*fact(bottom));
 
 const sublenComb = num => {
     let res = num-2;
-    if (res > 1) res = res+combNr((num-2),2);
+    if (res > 1) res += combNr((num-2),2);
     return res;
 }
 
-let mult = 1, ptr = 1;
-while (ptr < chain.length) {
-    let subLen = 1;
-    while ((chain[ptr]-chain[ptr-1]) == 1) {subLen++;ptr++;}
-    if (subLen >= 3) {
-        mult = mult*(1+sublenComb(subLen));
+const part2 = chain => {
+    let mult = 1, ptr = 1;
+    while (ptr < chain.length) {
+        let subLen = 1;
+        while ((chain[ptr]-chain[ptr-1]) == 1) {subLen++;ptr++;}
+        if (subLen >= 3) mult *= 1+sublenComb(subLen);
+        ptr++;
     }
-    ptr++;
+    return mult;
 }
 
-console.log('part 2', mult);
+let chain = constructChain(input);
+console.log('part 1', part1(chain));
+console.log('part 2', part2(chain));
