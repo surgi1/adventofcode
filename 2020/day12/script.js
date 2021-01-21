e781b783e49437d59bd549ef270b924167d7c6b8
Jan Surovec <gh1@cloudia.cz>
2021-01-21 16:30:45 +0100

    fixed html titles, re-added part1 into day 12 of 2020

--- 2020/day12/script.js
+++ 2020/day12/script.js
@@ -1,45 +1,57 @@
-// just part 2 here
-let start = {x:0,y:0}
-let ship = {
-    x:start.x,y:start.y,r:0
-}
-let waypoint = {
-    x: 10, y: -1
-}
+const dist = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 
-// [x*cos - y*sin, x*sin + y*cos]
-const rotate = (p, a) => {
-    let sin = Math.round(Math.sin(a*(Math.PI/180)));
-    let cos = Math.round(Math.cos(a*(Math.PI/180)));
-    return {
-        x: p.x*cos-p.y*sin,
-        y: p.x*sin+p.y*cos
+const part1 = input => {
+    const advanceShip = step => {
+        let action = step[0], value = parseInt(step.substr(1));
+        switch (action) {
+            case 'N': ship.y -= value; break;
+            case 'S': ship.y += value; break;
+            case 'E': ship.x += value; break;
+            case 'W': ship.x -= value; break;
+            case 'L': ship.r += value; break;
+            case 'R': ship.r -= value; break;
+            case 'F':
+                ship.x += value*(Math.round(Math.cos(ship.r*(Math.PI/180))));
+                ship.y -= value*(Math.round(Math.sin(ship.r*(Math.PI/180))));
+                break;
+        }
     }
+
+    let ship = {x:0,y:0,r:0}
+    input.map(step => advanceShip(step));
+    return dist(ship, {x:0,y:0});
 }
 
-const advanceShip = (step) => {
-    // parse step
-    let action = step[0];
-    let value = parseInt(step.substr(1));
-
-    switch (action) {
-        case 'N': waypoint.y = waypoint.y-value; break;
-        case 'S': waypoint.y = waypoint.y+value; break;
-        case 'E': waypoint.x = waypoint.x+value; break;
-        case 'W': waypoint.x = waypoint.x-value; break;
-        case 'L': waypoint = rotate(waypoint, 360-value); break;
-        case 'R': waypoint = rotate(waypoint, value); break;
-        case 'F': 
-            //ship.x = ship.x + value*(Math.round(Math.cos(ship.r*(Math.PI/180)))); // p1
-            //ship.y = ship.y - value*(Math.round(Math.sin(ship.r*(Math.PI/180)))); // p1
-            ship.x = ship.x + value*waypoint.x;
-            ship.y = ship.y + value*waypoint.y;
-            break;
+const part2 = input => {
+    const rotate = (p, a) => {
+        let sin = Math.sin(a*(Math.PI/180)),
+            cos = Math.cos(a*(Math.PI/180));
+        return {
+            x: Math.round(p.x*cos-p.y*sin),
+            y: Math.round(p.x*sin+p.y*cos)
+        }
     }
+
+    const advanceShip = step => {
+        let action = step[0], value = parseInt(step.substr(1));
+        switch (action) {
+            case 'N': waypoint.y -= value; break;
+            case 'S': waypoint.y += value; break;
+            case 'E': waypoint.x += value; break;
+            case 'W': waypoint.x -= value; break;
+            case 'L': waypoint = rotate(waypoint, 360-value); break;
+            case 'R': waypoint = rotate(waypoint, value); break;
+            case 'F': 
+                ship.x += value*waypoint.x;
+                ship.y += value*waypoint.y;
+                break;
+        }
+    }
+
+    let ship = {x:0,y:0}, waypoint = {x: 10, y: -1}
+    input.map(step => advanceShip(step));
+    return dist(ship, {x:0,y:0});
 }
 
-const dist = (a,b) => Math.abs(a.x-b.x)+Math.abs(a.y-b.y);
-
-input.map(step => advanceShip(step));
-
-console.log('ship', ship, 'dist from start', dist(ship, start));
+console.log('part 1', part1(input));
+console.log('part 2', part2(input));
