283c33a4f204056f89957f2c59cf850af75d969f
Jan Surovec <gh1@cloudia.cz>
2021-01-21 19:03:44 +0100

    generalized dimensions of GoL

--- 2020/day17/script.js
+++ 2020/day17/script.js
@@ -1,85 +1,58 @@
-// just the 4-d version for part 2
-let map = {}, steps = 6;
-
-const key = (x, y, z, w) => x+'_'+y+'_'+z+'_'+w;
-const setPoint = (map, x, y, z, w, state) => map[key(x, y, z, w)] = state;
+const key = p => p.join('_');
+const setPoint = (map, p, state) => map[key(p)] = state;
 const getCount = map => Object.values(map).length;
+const getPoint = (map, p) => map[key(p)] ? map[key(p)] : false
 
-const getPoint = (map, x, y, z, w) => {
-    let result = false;
-    let k = key(x, y, z, w);
-    if (map[k]) return map[k];
-    return false;
+const init = (input, map = {}) => {
+    input.map((line, y) => line.split('').map((v, x) => setPoint(map, [x,y,0,0], v == '#')))
+    return map;
 }
 
-const readInput = () => {
-    for (let y = 0; y < input.length; y++) {
-        let line = input[y];
-        for (let x = 0; x < line.length; x++) {
-            if (line[x] == '#') setPoint(map, x,y,0,0, true);
-        }
-    }
-}
-
-const getAdjacentLights = (map, xx, yy, zz, ww) => {
-    let count = 0;
-    for (let l = -1; l <= 1; l++) {
-        let w = ww+l;
-        for (let k = -1; k <= 1; k++) {
-            let z = zz+k;
-            for (let i = -1; i <= 1; i++) {
-                let y = yy+i;
-                for (let j = -1; j <= 1; j++) {
-                    let x = xx+j;
-                    if ((i == 0) && (j == 0) && (k == 0) && (l == 0)) continue;
-                    if (getPoint(map, x,y,z,w)) count++;
+const getAdjacentLights = (map, p, count = 0) => {
+    for (let w = -1; w <= 1; w++)
+        for (let z = -1; z <= 1; z++)
+            for (let y = -1; y <= 1; y++)
+                for (let x = -1; x <= 1; x++) {
+                    if ((y == 0) && (x == 0) && (z == 0) && (w == 0)) continue;
+                    if (getPoint(map, [p[0]+x, p[1]+y, p[2]+z, p[3]+w])) count++;
                 }
-            }
-        }
-    }
     return count;
 }
 
-const size = map => {
-    let min = {}, max = {}, keys = Object.keys(map), len = keys.length;
-    for (let i = 0; i < len; i++) {
-        if (!map[keys[i]]) continue;
-        let k = keys[i].split('_');
-        ['x','y','z','w'].map((coord, index) => {
-            if ((min[coord] == undefined) || k[index] < min[coord]) min[coord] = parseInt(k[index]);
-            if ((max[coord] == undefined) || k[index] > max[coord]) max[coord] = parseInt(k[index]);
+const getActiveBox = (map, dimensions) => {
+    let min = [], max = [];
+    Object.keys(map).filter(k => map[k] !== false).map(k => {
+        k.split('_').map(n => parseInt(n)).map((v, index) => {
+            if ((min[index] == undefined) || v < min[index]) min[index] = v;
+            if ((max[index] == undefined) || v > max[index]) max[index] = v;
         })
-    }
+    })
+    if (dimensions < 4) {min[3] = 1; max[3] = -1}
+    if (dimensions < 3) {min[2] = 1; max[2] = -1}
     return {min:min, max:max}
 }
 
-const nextState = lastState => {
-    let newState = {};
-    let dim = size(lastState);
-
-    for (let w = dim.min.w-1; w <= dim.max.w+1; w++) {
-        for (let z = dim.min.z-1; z <= dim.max.z+1; z++) {
-            for (let y = dim.min.y-1; y <= dim.max.y+1; y++) {
-                for (let x = dim.min.x-1; x <= dim.max.x+1; x++) {
-                    let lights = getAdjacentLights(lastState, x, y, z, w);
-                    // GOL rules
-                    if (getPoint(lastState, x,y,z,w) === true) {
-                        if (lights == 3 || lights == 2) setPoint(newState, x, y, z, w, true);
+const nextState = (lastState, dimensions) => {
+    let newState = {}, box = getActiveBox(lastState, dimensions);
+    for (let w = box.min[3]-1; w <= box.max[3]+1; w++)
+        for (let z = box.min[2]-1; z <= box.max[2]+1; z++)
+            for (let y = box.min[1]-1; y <= box.max[1]+1; y++)
+                for (let x = box.min[0]-1; x <= box.max[0]+1; x++) {
+                    let lights = getAdjacentLights(lastState, [x,y,z,w]);
+                    if (getPoint(lastState, [x,y,z,w]) === true) {
+                        if (lights == 3 || lights == 2) setPoint(newState, [x,y,z,w], true);
                     } else {
-                        if (lights == 3) setPoint(newState, x, y, z, w, true)
+                        if (lights == 3) setPoint(newState, [x,y,z,w], true);
                     }
-
                 }
-            }
-        }
-    }
-
     return newState;
 };
 
-readInput();
+const gameOfLife = (input, dimensions, steps = 6) => {
+    let newState = init(input);
+    for (let y = 0; y < steps; y++) newState = nextState(newState, dimensions);
+    return getCount(newState);
+}
 
-let newState = map;
-for (let i = 0; i < steps; i++) newState = nextState(newState);
-
-console.log('lights on', getCount(newState));
+console.log('part 1', gameOfLife(input, 3));
+console.log('part 2', gameOfLife(input, 4));
