3a7f4b50158e201c388b7b074813b04370fc0f2f
Jan Surovec <gh1@cloudia.cz>
2021-01-22 21:17:14 +0100

    last puzzle got back part 1 now

--- 2020/day19/script.js
+++ 2020/day19/script.js
@@ -1,28 +1,29 @@
-// just part 2
-// ended up with constructing the regexp approach
-let rules = [];
-
-const parseRules = () => {
+const parseRules = (rules = []) => {
     inputRules.map(rule => {
-        let tmp = {};
         let arr = rule.split(': ');
-        tmp.originalValue = arr[1];
-        if (arr[1].indexOf('"') > -1) tmp.originalValue = arr[1][1];
-        rules[arr[0]] = tmp;
+        rules[arr[0]] = arr[1].replace(/"/g, '');
     })
+    return rules;
 }
-
-parseRules();
 
 const combine = s => {
     let match = s.match(/\d+/g);
     while (match && match.length > 0) {
-        s = s.replace(match[0], '(' + rules[match[0]].originalValue + ')');
+        s = s.replace(match[0], '(' + rules[match[0]] + ')');
         match = s.match(/\d+/g);
     } 
     return s.replaceAll(' ', '');
 }
 
-let regexpStr = '^'+combine(rules[0].originalValue)+'$';
+const run = () => {
+    let regexpStr = '^'+combine(rules[0])+'$';
+    return input.filter(w => w.match(regexpStr)).length;
+}
 
-console.log(input.filter(w => w.match(regexpStr)).length);
+let rules = parseRules();
+console.log('part 1', run());
+
+rules[8] = '42+';
+rules[11] = '42 31|42 ( 42 31|42 ( 42 31|42 ( 42 31|42 ( 42 31 ) 31 ) 31 ) 31 ) 31';
+// ^ recursion applied 1 level more than was sufficient for my input; this can be automated if necessary
+console.log('part 2', run());
