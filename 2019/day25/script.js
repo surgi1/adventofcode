8d17a0adabf1469725a0b941f84abc316cdd86a6
Jan Surovec <gh1@cloudia.cz>
2021-01-29 21:40:34 +0100

    added map

--- 2019/day25/script.js
+++ 2019/day25/script.js
@@ -1,6 +1,78 @@
 // play the text game, it is lot of fun!
 // find 8 items, navigate to final location, export your journey up to that point into baseCommands, then power up the brute-force
-let root = $('#root'), commands = [], comp = new Computer(), invCount = 0, map = [], pos = {x:20, y:20};
+let root = $('#root'), commands = [], comp = new Computer(), invCount = 0, rooms = [], lastLoc = false, loc = false;
+const opDir = {north: 'south', south: 'north', west: 'east', east: 'west'};
+
+const drawRooms = rooms => {
+    let map = [], size = 10, pos = {x:size/2, y:size/2}, mapEl = $('.map');
+    rooms.map(r => delete r.drawn);
+    const dirs = {
+        north: {x: 0, y: -1},
+        south: {x: 0, y: 1},
+        east: {x: 1, y: 0},
+        west: {x: -1, y: 0}
+    }
+    const col = (map, x, res = []) => map.map(line => line[x])
+    const insCol = (map, x) => map.map((line, y) => map[y].splice(x, 0, undefined))
+    const roomClasses = room => Object.keys(dirs).filter(k => room[k] != undefined).join(' ')
+    const roomPos = id => {
+        for (let y = 0; y < map.length; y++)
+            for (let x = 0; x < map[y].length; x++) if (map[y][x]?.id == id) return {x: x, y: y}
+    }
+
+    const drawMap = () => {
+        mapEl.empty();
+        for (let y = 0; y < map.length; y++) {
+            for (let x = 0; x < map[y].length; x++) {
+                if (map[y] == undefined || map[y][x] == undefined || map[y][x] == '') continue;
+                let el = $('<div class="mappoint">', {css: {
+                    left: x*18+'px',
+                    top: y*18+'px'
+                }}).addClass(roomClasses(map[y][x]));
+                //el.html(map[y][x].id);
+                if (map[y][x].name == loc) el.html('*');
+                mapEl.append(el)
+            }
+        }
+    }
+
+    const drawCorridors = () => {
+        rooms.map(r => {
+            Object.keys(dirs).filter(k => r[k] != undefined).map(k => {
+                let p1 = roomPos(r.id);
+                if (r[k] > r.id) {
+                    let p2 = roomPos(r[k]);
+                    if (p2) mapEl.append('<svg><polyline points="'+ (p1.x*18-11) + ',' + p1.y*18 +' '+ (p2.x*18-11) + ',' + p2.y*18 +'"></polyline></svg>')
+                }
+            })
+        })
+    }
+
+    for (let y = 0; y < size; y++) map[y] = [];
+    let toPlace = [{room: rooms[0], fromDir: {x:0,y:0}}], ptr = 0;
+    while (ptr < toPlace.length) {
+        let r = toPlace[ptr], dest;
+        if (r.room.drawn) {ptr++; continue}
+        if (r.fromRoom) {
+            let p = roomPos(r.fromRoom.id);
+            dest = {x: p.x+r.fromDir.x, y: p.y+r.fromDir.y}
+            if (r.fromDir.x == 0) {
+                if (map[dest.y].filter(v => v != '' && v != undefined).length > 0) map.splice(dest.y, 0, []);
+            } else {
+                if (col(map, dest.x).filter(v => v != '' && v != undefined).length > 0) insCol(map, dest.x);
+            }
+        } else {
+            dest = {x: pos.x+r.fromDir.x, y: pos.y+r.fromDir.y}
+        }
+        map[dest.y][dest.x] = r.room;
+        r.room.drawn = true;
+        Object.entries(dirs).map(([k, v]) => r.room[k] != undefined && r.room[k] !== 999 && toPlace.push({room: rooms[r.room[k]], fromRoom: r.room, fromDir: v}));
+        ptr++;
+    }
+
+    drawMap();
+    drawCorridors();
+}
 
 const str2Command = s => {
     let res = [];
@@ -9,12 +81,25 @@ const str2Command = s => {
     return res;
 }
 
+const addLoc = (name, exits) => {
+    if (rooms.filter(r => r.name == name).length > 0) return rooms.filter(r => r.name == name)[0].id;
+    let id = rooms.length;
+    let tmp = {
+        id: rooms.length,
+        name: name
+    }
+    exits.map(e => tmp[e] = 999);
+    rooms.push(tmp);
+    return id;
+}
+
 const processOutput = s => {
-    let enabled = ['inv'], arr = [], items = false, drops = false, locChanged = false;
+    let enabled = ['inv'], arr = [], items = false, drops = false;
     s.split("\n").map(line => {
         if (line.indexOf('==') > -1) {
-            locChanged = true;
-            line = '<h3>'+line+'</h3>';
+            loc = line.substr(3, line.length-6);
+            line = '<h3>'+loc+'</h3>';
+            enabled = ['inv'];
         }
 
         if (line.indexOf('You take the') > -1) invCount++;
@@ -30,21 +115,31 @@ const processOutput = s => {
         if (line == 'Items in your inventory:') drops = [];
     })
 
-    if (locChanged) {
+    if (loc !== false) {
         $('[data-action=direct]').attr('disabled', true);
         enabled.map(id => $('#'+id).removeAttr('disabled'));
+        let id = addLoc(loc, enabled.filter(d => d != 'inv'));
+
+        // connect lastLoc
+        if (lastLoc !== false && loc != lastLoc) {
+            let lastLocId = rooms.filter(r => r.name == lastLoc)[0].id;
+            rooms[lastLocId][commands[commands.length-1]] = id;
+            rooms[id][opDir[commands[commands.length-1]]] = lastLocId;
+        }
+
+        lastLoc = loc;
+        drawRooms(rooms)
     }
 
     if (items !== false) items.map(i => arr.push(`<button class="temp" onclick="command('take `+i+`');$(this).attr('disabled',true);">`+i+` (take)</button>`))
     if (drops !== false) drops.map(i => arr.push(`<button class="temp" onclick="command('drop `+i+`');$(this).attr('disabled',true);">`+i+` (drop)</button>`))
-    $('#inv').html('Inventory (' + invCount + ')')
+    $('#inv').html('Inventory (' + invCount + ')');
     return '<div class="output">'+arr.join("<br>")+'</div>';
 }
 
 const tick = pars => {
     if (!pars) pars = [];
-    let res = comp.run(pars);
-    let s = '';
+    let res = comp.run(pars), s = '';
     res.output.map(code => s += String.fromCharCode(code));
     root.prepend(processOutput(s));
 }
