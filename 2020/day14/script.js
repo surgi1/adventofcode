ede493479c75dab06dfbdeaf9f4d62a9f574eaa9
Jan Surovec <gh1@cloudia.cz>
2021-02-14 21:24:08 +0100

    minor readability changes

--- 2020/day14/script.js
+++ 2020/day14/script.js
@@ -2,13 +2,10 @@ const part1 = () => {
     let maskAND, maskOR, mem = [];
     input.map(line => {
         if (line[0] == 'mask') {
-            let s1 = '0b' + line[1].replace(/X/g, '1');
-            let s0 = '0b' + line[1].replace(/X/g, '0');
-            maskAND = BigInt(s1);
-            maskOR = BigInt(s0);
-        } else {
+            maskAND = BigInt('0b' + line[1].replace(/X/g, '1'));
+            maskOR = BigInt('0b' + line[1].replace(/X/g, '0'));
+        } else
             mem[BigInt(line[1])] = BigInt(line[2]) & maskAND | maskOR;
-        }
     })
     console.log('part 1', Object.values(mem).reduce((pv, cv) => pv + cv, 0n));
 }
@@ -16,47 +13,23 @@ const part1 = () => {
 const part2 = () => {
     let mask = '', mem = {}, addresses = [];
 
+    const applyMask = s => s.split('').reduce((res, v, i) => res += (mask[i] == '0' ? v : mask[i]) , '')
     const generateMemAddresses = m => {
-        let i = m.indexOf('X');
-        if (i == -1) {
-            addresses.push(m);
-            return;
-        }
+        if (m.indexOf('X') == -1) return addresses.push(m);
         generateMemAddresses(m.replace('X', '1'));
         generateMemAddresses(m.replace('X', '0'));
-
     };
-
-    const applyMask = s => {
-        let res = '';
-        for (let i = 0; i < s.length; i++) {
-            if (mask[i] == '0') res = res + s[i];
-            else if (mask[i] == '1') res = res + '1';
-            else if (mask[i] == 'X') res = res + 'X';
-        }
-        return res;
-    }
-
-    const num2BinStr = n => {
-        let s = n.toString(2);
-        while (s.length < 36) s = '0'+s;
-        return s;    
-    }
 
     const fillMem = (baseAddress, value) => {
         // fill all mem addresses by permuting X in mask by value
         // first mask baseAddress, then permutate
         addresses = [];
-        generateMemAddresses(applyMask(num2BinStr(baseAddress)));
+        generateMemAddresses(applyMask(baseAddress.toString(2).padStart(36, '0')));
         addresses.map(a => mem['0b'+a] = value)
     }
 
     input.map(line => {
-        if (line[0] == 'mask') {
-            mask = line[1];
-        } else {
-            fillMem(parseInt(line[1]), parseInt(line[2]));
-        }
+        if (line[0] == 'mask') mask = line[1]; else fillMem(parseInt(line[1]), parseInt(line[2]));
     })
 
     console.log('part 2', Object.values(mem).reduce((pv, cv) => pv + cv, 0));
