390d89a2d26ecf3fdb4fb82e9e08dccf58540556
Jan Surovec <gh1@cloudia.cz>
2021-02-15 16:06:54 +0100

    readability changes

--- 2020/day20/script.js
+++ 2020/day20/script.js
@@ -2,11 +2,10 @@ const rotate = (src, out = Array.from(src)) => out.map((r,y) => r.map((p,x) => s
 
 const processImage = img => {
     const monster = ['00000000000000000010', '10000110000110000111', '01001001001001001000'].map(l => l.split(''))
-    const chars = (arr, ch = '1') => arr.join('').split('').filter(c => c == ch).length
-    const isMonsterAt = (sx, sy) => monster.every((l,y) => l.every((c,x) => '0' == c || ('1' == c && ['1','2'].includes(img[sy+y][sx+x]))))
-    const colorMonsterAt = (sx, sy) => monster.map((l,y) => l.map((c,x) => img[sy+y][sx+x] = c == '1' ? '2' : img[sy+y][sx+x]))
-    const monsters = () => img.filter((l,y) => img[y+2]).reduce((a,l,y) => a+l.filter((c,x) => isMonsterAt(x,y) && colorMonsterAt(x,y)).length, 0)
-    const drawImage = (s = '') => {
+    const isMonsterAt = (sx, sy) => monster.every((l,y) => l.every((c,x) => c == '0' || (c == '1' && ['1','2'].includes(img[sy+y][sx+x]))))
+    const markMonsterAt = (sx, sy) => monster.map((l,y) => l.map((c,x) => img[sy+y][sx+x] = c == '1' ? '2' : img[sy+y][sx+x]))
+    const monsters = () => img.filter((l,y) => img[y+2]).reduce((a,l,y) => a+l.filter((c,x) => isMonsterAt(x,y) && markMonsterAt(x,y)).length, 0)
+    const draw = (s = '') => {
         img.map((l,y) => l.map((p,x) => s += p == 0 ? '' : `<div class="point type_${p}" style="left:${x*6}px;top:${y*6}px"> </div>`))
         document.getElementById('root').innerHTML = s;
     }
@@ -14,8 +13,8 @@ const processImage = img => {
     for (let i = 0; i < 3; i++) if (!monsters()) img = rotate(img);
     if (!monsters()) img.reverse(); // flip
     while (!monsters()) img = rotate(img);
-    drawImage();
-    return chars(img);
+    draw();
+    return img.reduce((a,l) => a+l.filter(e => e == '1').length, 0);
 }
 
 console.log('water roughness', processImage(new TileSet(input).process()));
