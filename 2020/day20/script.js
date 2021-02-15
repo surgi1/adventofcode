5f922ea622068cb8b1adf61b53472bc318d01f2f
Jan Surovec <gh1@cloudia.cz>
2021-02-15 17:26:00 +0100

    readability changes, minor

--- 2020/day20/script.js
+++ 2020/day20/script.js
@@ -5,15 +5,12 @@ const processImage = img => {
     const isMonsterAt = (sx, sy) => monster.every((l,y) => l.every((c,x) => c == '0' || (c == '1' && ['1','2'].includes(img[sy+y][sx+x]))))
     const markMonsterAt = (sx, sy) => monster.map((l,y) => l.map((c,x) => img[sy+y][sx+x] = c == '1' ? '2' : img[sy+y][sx+x]))
     const monsters = () => img.filter((l,y) => img[y+2]).reduce((a,l,y) => a+l.filter((c,x) => isMonsterAt(x,y) && markMonsterAt(x,y)).length, 0)
-    const draw = (s = '') => {
-        img.map((l,y) => l.map((p,x) => s += p == 0 ? '' : `<div class="point type_${p}" style="left:${x*6}px;top:${y*6}px"> </div>`))
-        document.getElementById('root').innerHTML = s;
-    }
+    const genHtml = () => img.reduce((s,l,y) => s+l.reduce((s2,p,x) => s2 += p == 0 ? '' : `<div class="p t${p}" style="left:${x*6}px;top:${y*6}px"></div>`, ''), '')
 
     for (let i = 0; i < 3; i++) if (!monsters()) img = rotate(img);
     if (!monsters()) img.reverse(); // flip
     while (!monsters()) img = rotate(img);
-    draw();
+    document.getElementById('root').innerHTML = genHtml();
     return img.reduce((a,l) => a+l.filter(e => e == '1').length, 0);
 }
 
