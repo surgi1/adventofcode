447497ac0ffb1155a0b8ed387ec71eb406564b01
Jan Surovec <gh1@cloudia.cz>
2021-12-09 11:38:44 +0100

    readability changes

--- 2021/day9/script.js
+++ 2021/day9/script.js
@@ -2,41 +2,37 @@ let map = [], cols = input[0].length, rows = input.length;
 
 input.map((line, i) => map[i] = line.split('').map(n => parseInt(n)));
 
-const part1 = (lowpoints = []) => {
-    const adjacent = (x, y, res = []) => {
-        if (x > 0) res.push(map[y][x-1]);
-        if (y > 0) res.push(map[y-1][x]);
-        if (x < cols-1) res.push(map[y][x+1]);
-        if (y < rows-1) res.push(map[y+1][x]);
-        return res;
-    }
-
-    for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) {
-        if (Math.min(...adjacent(x, y)) > map[y][x]) lowpoints.push({x: x, y: y})
-    }
-
-    console.log(lowpoints.reduce((acc, p) => acc+map[p.y][p.x]+1, 0));
-    return lowpoints;
+const adjacent = (x, y, res = []) => {
+    if (x > 0) res.push({x: x-1, y: y});
+    if (y > 0) res.push({x: x, y: y-1});
+    if (x < cols-1) res.push({x: x+1, y: y});
+    if (y < rows-1) res.push({x: x, y: y+1});
+    return res;
 }
 
-const part2 = lowpoints => {
+const part1 = (lowPoints = []) => {
+    for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) 
+        if (Math.min(...adjacent(x, y).map(p => map[p.y][p.x])) > map[y][x]) lowPoints.push({x: x, y: y})
+
+    console.log(lowPoints.reduce((acc, p) => acc+map[p.y][p.x]+1, 0));
+    return lowPoints;
+}
+
+const part2 = lowPoints => {
     const spread = (x, y) => {
         if (map[y][x] < 9) basinSize++; else return;
         map[y][x] = 9;
-        if (x > 0) spread(x-1, y);
-        if (y > 0) spread(x, y-1);
-        if (x < cols-1) spread(x+1, y);
-        if (y < rows-1) spread(x, y+1);
+        adjacent(x, y).map(p => spread(p.x, p.y));
     }
 
     let basinSizes = [], basinSize = 0;
-    lowpoints.map(p => {
+    lowPoints.map(p => {
         basinSize = 0;
         spread(p.x, p.y);
         basinSizes.push(basinSize);
     })
 
-    return basinSizes.sort((a, b) => b-a).splice(0, 3).reduce((acc, n) => acc*n, 1);
+    return basinSizes.sort((a, b) => b-a).slice(0, 3).reduce((acc, n) => acc*n, 1);
 }
 
 console.log(part2(part1()));
