ec1e24fe0cc40b05aff4a07941fb6c308e0cb3b3
Jan Surovec <gh1@cloudia.cz>
2021-12-09 08:56:54 +0100

    readability changes

--- 2021/day9/script.js
+++ 2021/day9/script.js
@@ -1,41 +1,42 @@
-let map = [];
+let map = [], cols = input[0].length, rows = input.length;
 
 input.map((line, i) => map[i] = line.split('').map(n => parseInt(n)));
 
-let cols = map[0].length, rows = map.length, lowpointsScore = 0, lowpoints = [];
-
-const adjacent = (x, y) => {
-    let res = [];
-    if (x > 0) res.push(map[y][x-1]);
-    if (y > 0) res.push(map[y-1][x]);
-    if (x < cols-1) res.push(map[y][x+1]);
-    if (y < rows-1) res.push(map[y+1][x]);
-    return res;
-}
-
-for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) {
-    if (Math.min(...adjacent(x, y)) > map[y][x]) {
-        lowpointsScore += map[y][x]+1;
-        lowpoints.push({x: x, y: y});
+const part1 = (lowpoints = []) => {
+    const adjacent = (x, y, res = []) => {
+        if (x > 0) res.push(map[y][x-1]);
+        if (y > 0) res.push(map[y-1][x]);
+        if (x < cols-1) res.push(map[y][x+1]);
+        if (y < rows-1) res.push(map[y+1][x]);
+        return res;
     }
+
+    for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) {
+        if (Math.min(...adjacent(x, y)) > map[y][x]) lowpoints.push({x: x, y: y})
+    }
+
+    console.log(lowpoints.reduce((acc, p) => acc+map[p.y][p.x]+1, 0));
+    return lowpoints;
 }
 
-console.log(lowpointsScore);
+const part2 = lowpoints => {
+    const spread = (x, y) => {
+        if (map[y][x] < 9) basinSize++; else return;
+        map[y][x] = 9;
+        if (x > 0) spread(x-1, y);
+        if (y > 0) spread(x, y-1);
+        if (x < cols-1) spread(x+1, y);
+        if (y < rows-1) spread(x, y+1);
+    }
 
-const spread = (x, y) => {
-    if (map[y][x] < 9) basinSize++; else return;
-    map[y][x] = 9;
-    if (x > 0) spread(x-1, y);
-    if (y > 0) spread(x, y-1);
-    if (x < cols-1) spread(x+1, y);
-    if (y < rows-1) spread(x, y+1);
+    let basinSizes = [], basinSize = 0;
+    lowpoints.map(p => {
+        basinSize = 0;
+        spread(p.x, p.y);
+        basinSizes.push(basinSize);
+    })
+
+    return basinSizes.sort((a, b) => b-a).splice(0, 3).reduce((acc, n) => acc*n, 1);
 }
 
-let basinSizes = [], basinSize = 0;
-lowpoints.map(p => {
-    basinSize = 0;
-    spread(p.x, p.y);
-    basinSizes.push(basinSize);
-})
-
-console.log(basinSizes.sort((a, b) => b-a).splice(0, 3).reduce((acc, n) => acc*n, 1));
+console.log(part2(part1()));
