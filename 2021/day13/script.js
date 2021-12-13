68774b3d8316c2dfa71df27357d42b8ce7108cb8
Jan Surovec <gh1@cloudia.cz>
2021-12-13 20:46:31 +0100

    readability changes

--- 2021/day13/script.js
+++ 2021/day13/script.js
@@ -1,41 +1,35 @@
-const initScreen = (data, xs = [], ys = [], screen = []) => {
-    data.map(l => {
-        let tmp = l.split(',').map(n => parseInt(n));
-        xs.push(tmp[0]); ys.push(tmp[1]);
-    })
-    for (let y = 0, rows = Math.max(...ys), cols = Math.max(...xs); y <= rows; y++) screen[y] = Array(cols+1).fill(0);
-    ys.map((y, i) => screen[y][xs[i]] = 1);
+const initScreen = (data, screen = []) => {
+    let pts = data.map(l => l.split(',').map(n => parseInt(n))),
+        dim = [0,1].map(k => Math.max(...pts.map(v => v[k]))+1);
+    screen = Array.from({length: dim[1]}, v => Array(dim[0]).fill(0));
+    pts.map(p => screen[p[1]][p[0]] = 1);
     return screen;
 }
 
-const initFolds = (data, folds = []) => {
-    data.map(l => {
-        let tmp = l.split('=');
-        folds.push({axis: tmp[0], val: parseInt(tmp[1])})
-    })
-    return folds;
-}
+const folds = data => data.map(l => l.split('=').map((v, k) => !k ? v : parseInt(v)))
 
 const fold = (oldScreen, rule, foldId, screen = []) => {
     oldScreen.forEach((row, y) => {
-        if (rule.axis == 'x') screen[y] = Array(rule.val).fill(0); else {
-            if (y < rule.val) screen[y] = Array(oldScreen[0].length).fill(0);
-        }
+        if (rule[0] == 'x')
+            screen[y] = Array(rule[1]).fill(0);
+        else if (y < rule[1])
+            screen[y] = Array(oldScreen[0].length).fill(0);
+
         row.forEach((d, x) => {
             if (d == 0) return;
-            if (rule.axis == 'x') {
-                if (x < rule.val) screen[y][x] = 1; else screen[y][oldScreen[0].length-x-1] = 1;
+            if (rule[0] == 'x') {
+                if (x < rule[1]) screen[y][x] = 1; else screen[y][oldScreen[0].length-x-1] = 1;
             } else {
-                if (y < rule.val) screen[y][x] = 1; else screen[oldScreen.length-y-1][x] = 1;
+                if (y < rule[1]) screen[y][x] = 1; else screen[oldScreen.length-y-1][x] = 1;
             }
         })
     })
 
-    if (foldId == 0) console.log(screen.reduce((a, b) => a.concat(b)).reduce((acc, p) => acc+p));
+    if (!foldId) console.log(screen.reduce((n, m) => n.concat(m)).reduce((a, p) => a+p));
     return screen;
 }
 
-let screen = initScreen(screenData), folds = initFolds(foldData);
-folds.map((f, i) => screen = fold(screen, f, i));
+let screen = initScreen(screenData);
+folds(foldData).map((f, i) => screen = fold(screen, f, i));
 
 console.log(screen.map((row, y) => row.reduce((p, x) => p + (x == 1 ? '#' : ' '), '')));
