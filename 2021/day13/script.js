4f68459669fba8bcafd812beb10aa1daf3878053
Jan Surovec <gh1@cloudia.cz>
2021-12-13 23:52:52 +0100

    readability changes

--- 2021/day13/script.js
+++ 2021/day13/script.js
@@ -8,28 +8,22 @@ const initScreen = (data, screen = []) => {
 
 const folds = data => data.map(l => l.split('=').map((v, k) => !k ? v : parseInt(v)))
 
-const fold = (oldScreen, rule, foldId, screen = []) => {
-    oldScreen.forEach((row, y) => {
-        if (rule[0] == 'x')
-            screen[y] = Array(rule[1]).fill(0);
-        else if (y < rule[1])
-            screen[y] = Array(oldScreen[0].length).fill(0);
+const fold = (prev, f, fId) => {
+    let screen = Array.from({length: f[0]=='y' ? f[1] : prev.length},
+        v => Array(f[0]=='x' ? f[1] : prev[0].length).fill(0));
 
-        row.forEach((d, x) => {
-            if (d == 0) return;
-            if (rule[0] == 'x') {
-                if (x < rule[1]) screen[y][x] = 1; else screen[y][oldScreen[0].length-x-1] = 1;
-            } else {
-                if (y < rule[1]) screen[y][x] = 1; else screen[oldScreen.length-y-1][x] = 1;
-            }
-        })
-    })
+    prev.forEach((row, y) => row.forEach((d, x) => {
+        if (d == 0) return;
+        let _x = f[0] == 'x' && x > f[1] ? prev[0].length-x-1 : x;
+        let _y = f[0] == 'y' && y > f[1] ? prev.length-y-1 : y;
+        screen[_y][_x] = 1;
+    }))
 
-    if (!foldId) console.log(screen.reduce((n, m) => n.concat(m)).reduce((a, p) => a+p));
+    if (!fId) console.log(screen.reduce((n, m) => n.concat(m)).reduce((a, p) => a+p));
     return screen;
 }
 
 let screen = initScreen(screenData);
 folds(foldData).map((f, i) => screen = fold(screen, f, i));
 
-console.log(screen.map((row, y) => row.reduce((p, x) => p + (x == 1 ? '#' : ' '), '')));
+console.log(screen.map((row, y) => row.reduce((p, x) => p + (x == 1 ? '██' : '  '), '')));
