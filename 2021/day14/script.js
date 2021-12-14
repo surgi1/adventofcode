a09a6cd109e101a0e8639bb89a49fa4f5cf40f2f
Jan Surovec <gh1@cloudia.cz>
2021-12-15 00:21:49 +0100

    aoc 2021 day 14 part 2

--- 2021/day14/script.js
+++ 2021/day14/script.js
@@ -1,59 +1,24 @@
-/*let input = 'NNCB';
-let synt = {
-'CH': 'B',
-'HH': 'N',
-'CB': 'H',
-'NH': 'C',
-'HB': 'C',
-'HC': 'B',
-'HN': 'C',
-'NN': 'C',
-'BH': 'H',
-'NC': 'B',
-'NB': 'B',
-'BN': 'B',
-'BB': 'N',
-'BC': 'B',
-'CC': 'N',
-'CN': 'C'
-}
-*/
-let nodes = [];
+const run = steps => {
+    let pairs = {}, freq = {};
 
-const addNode = params => nodes.push({...params, id: nodes.length})-1
+    for (let i = 1; i < input.length; i++) pairs[input[i-1]+input[i]] = 1;
 
-input.split('').map((l, i) => addNode({
-    l:l,
-    left: i > 0 ? i-1 : undefined,
-    right: i < input.length-1 ? i+1 : undefined,
-}))
+    Object.keys(synt).map(k => pairs[k] = pairs[k] || 0);
 
-const step = () => {
-    let currentNode = nodes[nodes[0].right];
-    while (currentNode != undefined) {
-        let from = nodes[currentNode.left].l+currentNode.l;
-        if (synt[from]) {
-            let newNodeId = addNode({l: synt[from], left: nodes[currentNode.left].id, right: currentNode.id});
-            nodes[currentNode.left].right = newNodeId;
-            currentNode.left = newNodeId;
-        }
-        currentNode = nodes[currentNode.right];
-    }
-}
-
-const freq = () => {
-    let freq = {};
-    nodes.forEach(n => {
-        if (!freq[n.l]) freq[n.l] = {l:n.l, count:0};
-        freq[n.l].count++;
+    for (let i = 0; i < steps; i++) Object.entries(pairs).map(([k, v]) => {
+        let [a, c] = k.split(''), b = synt[k];
+        pairs[a+c] -= v;
+        pairs[a+b] += v;
+        pairs[b+c] += v;
     })
-    console.log(Object.values(freq).sort((a, b) => a.count-b.count));
+
+    Object.entries(pairs).map(([k, v]) => freq[k[0]] = freq[k[0]]+v || v)
+
+    freq[input[input.length-1]]++; // last letter
+
+    let sorted = Object.values(freq).sort((a, b) => b-a);
+    return sorted[0]-sorted[sorted.length-1];
 }
 
-const display = (s = '', nId = 0) => {
-    while (nodes[nId].right != undefined) {s += nodes[nId].l; nId = nodes[nId].right}
-    return s+nodes[nId].l
-}
-
-for (let i = 0; i < 40; i++) step();
-//freq();
+console.log(run(10));
+console.log(run(40));
