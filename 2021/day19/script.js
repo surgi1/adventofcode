7ebffb6c7c0b9d1b988c74cd5d47ebcb5cb31e96
Jan Surovec <gh1@cloudia.cz>
2021-12-19 17:55:07 +0100

    basic readability changes

--- 2021/day19/script.js
+++ 2021/day19/script.js
@@ -1,10 +1,13 @@
 const dist = (a,b, res = 0) => a.reduce((res, val, i) => res += (val-b[i])*(val-b[i]), 0)
-const distFromProbe = (probes, probeId) => probes.map(p => dist(probes[probeId], p))
+const distFromBeacon = (beacons, id) => beacons.map(p => dist(beacons[id], p))
 const matchArr = (a, b) => a.reduce((res, e) => res+(b.includes(e) ? 1 : 0), 0);
 const beaconId = (scannerId, beaconNr) => 'b_'+scannerId+'_'+beaconNr;
 const distinct = (value, index, self) => self.indexOf(value) === index;
-const nextXformKey = b => Object.keys(xforms).filter(k => k.indexOf('_'+b+'_') > -1)[0];
-const mDist = (a,b) => a.reduce((acc, v, i) => acc += Math.abs(v-b[i]), 0)
+const nextTransformKey = b => Object.keys(baseTransforms).filter(k => k.indexOf('_'+b+'_') > -1)[0];
+const manhattanDist = (a,b) => a.reduce((acc, v, i) => acc += Math.abs(v-b[i]), 0)
+const vectorMatrixProduct = (v, matrix) => v.map((e, i) => v[0]*matrix[0][i]+v[1]*matrix[1][i]+v[2]*matrix[2][i]);
+const vectorSub = (v1, v2) => v2.map((e, i) => e-v1[i]);
+const vectorAdd = (v1, v2) => v2.map((e, i) => e+v1[i]);
 
 const matchScanners = (a, b, res = 0) => {
     for (let i = 0; i < scannerMeasures[a].length; i++) for (let j = 0; j < scannerMeasures[b].length; j++)
@@ -14,118 +17,79 @@ const matchScanners = (a, b, res = 0) => {
 
 const markDuplicates = (a, b) => {
     for (let i = 0; i < scannerMeasures[a].length; i++) for (let j = 0; j < scannerMeasures[b].length; j++) {
-        if (matchArr(scannerMeasures[a][i], scannerMeasures[b][j]) >= 3) {
-            // mark all matches from bj
-            scannerMeasures[b][j].map((d, probeId) => {
-                let ind = scannerMeasures[a][i].indexOf(d);
-                if (ind > -1) beacons[beaconId(b, probeId)] = beacons[beaconId(a, ind)];
-            })
-            break;
-        }
+        if (matchArr(scannerMeasures[a][i], scannerMeasures[b][j]) < 3) continue;
+        // mark all matches from bj
+        scannerMeasures[b][j].map((d, id) => {
+            let index = scannerMeasures[a][i].indexOf(d);
+            if (index > -1) beacons[beaconId(b, id)] = beacons[beaconId(a, index)];
+        })
     }
 }
 
-const constructTransMatrix = (va, vb) => {
+const constructTransformMatrix = (va, vb) => {
     let res = Array.from({length:3}, e => Array(3).fill(0))
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (va[i] == vb[j]) res[i][j] = 1;
-            if (va[i] == -vb[j]) res[i][j] = -1;
-        }
+    for (let i = 0; i < 3; i++) for (let j = 0; j < 3; j++) {
+        if (va[i] == vb[j]) res[i][j] = 1;
+        if (va[i] == -vb[j]) res[i][j] = -1;
     }
-    return res;
-}
-
-const vecXmatrix = (v, matrix) => {
-    let res = [];
-    for (let i = 0; i < 3; i++) {
-        res[i] = v[0]*matrix[0][i]+v[1]*matrix[1][i]+v[2]*matrix[2][i];
-    }
-    return res;
-}
-
-const vecMinusVec = (v1, v2, res = []) => {
-    for (c = 0; c < 3; c++) res[c] = v2[c]-v1[c];
-    return res;
-}
-
-const vecPlusVec = (v1, v2, res = []) => {
-    for (c = 0; c < 3; c++) res[c] = v2[c]+v1[c];
     return res;
 }
 
 const determineScannerPosition = (a, b) => {
     if (scanners[a] === undefined && Array.isArray(scanners[b])) scanners[a] = scanners[b].slice(); else return;
-    let found = false;
     for (let i = 0; i < scannerMeasures[a].length; i++) {
         for (let j = 0; j < scannerMeasures[b].length; j++) {
-            if (matchArr(scannerMeasures[a][i], scannerMeasures[b][j]) >= 12) {
-                found = true;
-                // mark all matches from bj
-                let pa = [], pb = [], va = [], vb = [];
-                scannerMeasures[b][j].map((d, probeId) => {
-                    let ind = scannerMeasures[a][i].indexOf(d);
-                    if (ind > -1) {
-                        pa.push(input[a][ind].slice());
-                        pb.push(input[b][probeId].slice());
-                    }
-                })
-                va = vecMinusVec(pa[1], pa[0]);
-                vb = vecMinusVec(pb[1], pb[0]);
-                let xform = constructTransMatrix(va, vb);
-                let pa0_in_b = vecXmatrix(pa[0], xform);
-
-                xforms['_'+a+'_'+b] = xform.slice();
-
-                scanners[a] = vecMinusVec(pa0_in_b, pb[0]);
-                translate['_'+a+'_'+b] = scanners[a].slice();
-
-                // need to get from b to 0
-                while (b != 0) {
-                    let k = nextXformKey(b), tmp = k.split('_').map(n => parseInt(n));
-                    scanners[a] = vecPlusVec(vecXmatrix(scanners[a], xforms[k]), translate[k]);
-                    b = tmp[2];
+            if (matchArr(scannerMeasures[a][i], scannerMeasures[b][j]) < 12) continue;
+            // mark all matches from bj
+            let pa = [], pb = [];
+            scannerMeasures[b][j].map((d, id) => {
+                let index = scannerMeasures[a][i].indexOf(d);
+                if (index > -1) {
+                    pa.push(input[a][index].slice());
+                    pb.push(input[b][id].slice());
                 }
-                //console.log('relative', a, 'to', b, scanners[a]);
-                break;
+            })
+            let xform = constructTransformMatrix(vectorSub(pa[1], pa[0]), vectorSub(pb[1], pb[0]));
+            let pa0InBCoords = vectorMatrixProduct(pa[0], xform);
+
+            baseTransforms['_'+a+'_'+b] = xform.slice();
+            scanners[a] = vectorSub(pa0InBCoords, pb[0]);
+            translations['_'+a+'_'+b] = scanners[a].slice(); // save this translation for later use
+
+            // need to get from b to 0
+            while (b != 0) {
+                let k = nextTransformKey(b), tmp = k.split('_');
+                scanners[a] = vectorAdd(vectorMatrixProduct(scanners[a], baseTransforms[k]), translations[k]);
+                b = tmp[2];
             }
         }
-        if (found) break;
     }
 }
 
-let scanners = [[0,0,0]], xforms = {}, translate = {}, groups = [], beacons = {}, uuid = 0;
+let scanners = [[0,0,0]], baseTransforms = {}, translations = {}, beacons = {}, uuid = 0;
 input.map((probes, scannerId) => probes.map((coords, beaconNr) => beacons[beaconId(scannerId, beaconNr)] = uuid++))
 
-let scannerMeasures = input.map((probeData, scannerId) => input[scannerId].map((p, i) => distFromProbe(input[scannerId], i)))
+let scannerMeasures = input.map((beaconsData, scannerId) => input[scannerId].map((p, i) => distFromBeacon(input[scannerId], i)))
 
-for (let i = 0; i < input.length; i++) {
-    groups[i] = [];
-    for (let j = 0; j < input.length; j++) {
-        if (i==j) continue;
-        let m = matchScanners(i, j);
-        if (m >= 12) groups[i].push(j);
-        if (m >= 3 && i>j) markDuplicates(i, j);
-        if (m >= 12) determineScannerPosition(i, j);
-    }
-}
-
-while (scanners.filter(s => Array.isArray(s)).length < input.length) {
-    for (let i = 0; i < input.length; i++) if (scanners[i] == undefined) {
-        for (let j = 0; j < input.length; j++) {
-            if (i==j) continue;
-            let m = matchScanners(i, j);
-            if (m >= 12) determineScannerPosition(i, j);
-        }
-    }
+for (let i = 0; i < input.length; i++) for (let j = 0; j < input.length; j++) {
+    if (i==j) continue;
+    markDuplicates(i, j);
+    if (matchScanners(i, j) >= 12) determineScannerPosition(i, j);
 }
 
 console.log(Object.values(beacons).filter(distinct).length); // part 1
 
+// unnecessarily greedy part to finish finsing the scanners positions
+while (scanners.filter(s => Array.isArray(s)).length < input.length) {
+    for (let i = 0; i < input.length; i++) if (scanners[i] == undefined) {
+        for (let j = 0; j < input.length; j++) if (i != j && matchScanners(i, j) >= 12) determineScannerPosition(i, j);
+    }
+}
+
 let maxDist = 0;
 for (let i = 0; i < scanners.length; i++) for (let j = i+1; j < scanners.length; j++) {
     if (i==j) continue;
-    maxDist = Math.max(maxDist, mDist(scanners[i], scanners[j]));
+    maxDist = Math.max(maxDist, manhattanDist(scanners[i], scanners[j]));
 }
 
-console.log(maxDist)
+console.log(maxDist) // part 2
