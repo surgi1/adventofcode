ad1810ba93daff2e1f22ca81939a28c301c4ba73
Jan Surovec <honza@Jans-MacBook-Pro.local>
2021-12-20 14:14:18 +0100

    reworked solution to be less greedy

--- 2021/day20/script.js
+++ 2021/day20/script.js
@@ -1,29 +1,24 @@
-// BEWARE! This solution is fundamentally wrong, it works only by the computation range being expanded so wildly so it covers for the error
-// To correct it, treat the void data (the pixels our of range in prev step, thus never directly computed) not as dark, but as stepNr % 2
-// as they tend to flash due to pattern[0]=1 and pattern[511] = 0
-
-const key = (x,y) => '_'+x+'_'+y;
 const run = steps => {
-    const step = () => {
-        let newMap = {};
+    const key = (x,y) => x+'_'+y;
+    const step = (stepNr, newMap = {}) => {
         for (let y = min;y <= max; y++) for (let x = min; x <= max; x++) {
             let s = '';
-            for (let i = -1; i <= 1; i++) for (let j = -1; j <= 1; j++) s += (map[key(x+j, y+i)] == 1) ? '1' : '0';
+            for (let i = -1; i <= 1; i++) for (let j = -1; j <= 1; j++)
+                s += (map[key(x+j, y+i)] !== undefined) ? map[key(x+j, y+i)] : stepNr % 2;
             let patternIndex = parseInt(s, 2);
-            if (pattern[patternIndex] == '#') newMap[key(x, y)] = 1;
+            newMap[key(x, y)] = pattern[patternIndex] == '#' ? 1 : 0;
         }
-        min -= 1; max += 1;
+        min--; max++;
         map = newMap;
     }
 
-    let map = {}, min = -100, max = input.length+100, sum = 0;
+    let map = {}, min = -1, max = input.length;
 
-    input.map((line, y) => line.split('').map((d, x) => map[key(x,y)] = (d == '#' ? 1 : undefined)))
+    input.map((line, y) => line.split('').map((d, x) => map[key(x,y)] = (d == '#' ? 1 : 0)))
 
-    for (let i = 0; i < steps; i++) step();
-    for (let y = -steps;y < input.length+steps; y++) for (let x = -steps; x < input.length+steps; x++) if (map[key(x,y)] == 1) sum++;
-    console.log(sum);
+    for (let i = 0; i < steps; i++) step(i);
+    return Object.values(map).reduce((acc, v) => acc+v, 0);
 }
 
-run(2)
-run(50)
+console.log(run(2));
+console.log(run(50));
