ceea2d537a86c5119235a071640be80a3bad2c3e
Jan Surovec <gh1@cloudia.cz>
2021-12-25 11:22:33 +0100

    added visuals for day 25 of aoc 2021

--- 2021/day25/script.js
+++ 2021/day25/script.js
@@ -1,6 +1,25 @@
-let map = {}, dim = [input.length, input[0].length], steps = 0;
+let map = {}, dim = [input.length, input[0].length], steps = 0, root = document.getElementById('root');
 
 input.map((r, y) => r.split('').map((c, x) => (c != '.') && (map[y+'_'+x] = c)))
+
+const drawInit = (a = [], s = '') => {
+    for (let y = 0; y < dim[0]; y++) for (let x = 0; x < dim[1]; x++)
+        s +=  `<div id="${y}_${x}" class="pt" style="left:${x*3}px;top:${y*3}px"></div>`;
+    root.innerHTML = s;
+}
+
+const draw = (a = []) => {
+    Object.entries(map).map(([k, v]) => {
+        let coords = k.split('_').map(n => n*1);
+        if (!a[coords[0]]) a[coords[0]] = Array(dim[1]).fill(0);
+        a[coords[0]][coords[1]] = v;
+    })
+    for (let y = 0; y < dim[0]; y++) for (let x = 0; x < dim[1]; x++) {
+        let e = document.getElementById(y+'_'+x);
+        e.classList.remove('pt1', 'pt2');
+        if (a[y][x] && a[y][x] != 0) e.classList.add('pt'+(a[y][x] == 'v' ? '1' : '2'));
+    }
+}
 
 const step = (moved = 0) => {
     const moveInDir = (c, vect, newMap = {}) => {
@@ -19,6 +38,10 @@ const step = (moved = 0) => {
     return moved;
 }
 
-while (step() != 0) steps++;
+const tick = () => {
+    draw(); steps++;
+    if (step() != 0) setTimeout(tick, 0); else console.log(steps);
+}
 
-console.log(steps+1);
+drawInit();
+tick();
