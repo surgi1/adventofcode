ddc90639b94c9cf0eea7d51e671730949126f932
Jan Surovec <gh1@cloudia.cz>
2022-01-07 19:16:21 +0100

    readability changes

--- 2015/day19/script.js
+++ 2015/day19/script.js
@@ -1,41 +1,25 @@
-let variants = [], start = 'e', replacements = [], steps = 0, s = input;
+let variants = [], steps = 0, s = input;
 
-const readInput = () => {
-    inputReplacements.map(r => {
-        let parsed = r.split(' => ');
-        replacements.push({from: parsed[0], to: parsed[1]});
-    })
-}
-
-const generateVariants = (s, r) => {
-    let i = 0;
-    while(s.indexOf(r.from, i) > -1) {
+const generateVariants = (s, r, i = 0) => {
+    while (s.indexOf(r.from, i) > -1) {
         let vs = s.substr(0,s.indexOf(r.from, i), r.from.length) + r.to + s.substr(s.indexOf(r.from, i)+r.from.length)
         if (!variants.includes(vs)) variants.push(vs);
         i++;
     }
 }
 
-const generateAllVariants = sArr => {
-    variants = [];
-    sArr.map(s => {
-        if (s.length <= input.length) replacements.map(r => generateVariants(s, r))
-    })    
-}
-
 const repeatedlyApplyReversed = (s, r) => {
-    while(s.indexOf(r.to) > -1) {
-        let vs = s.substr(0,s.indexOf(r.to), r.to.length) + r.from + s.substr(s.indexOf(r.to)+r.to.length);
-        s = vs;
+    while (s.indexOf(r.to) > -1) {
+        s = s.substr(0,s.indexOf(r.to), r.to.length) + r.from + s.substr(s.indexOf(r.to)+r.to.length);
         steps++;
     }
     return s;
 }
 
-readInput();
+let replacements = inputReplacements.map(r => new Object({from: r.split(' => ')[0], to: r.split(' => ')[1]}))
 
 // p1
-generateAllVariants([input]);
+replacements.map(r => generateVariants(input, r));
 console.log('part 1', variants.length);
 
 // p2 - reduce way
