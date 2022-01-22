d90cfb0731abb3f77c01dd881316202df07b9c05
Jan Surovec <gh1@cloudia.cz>
2022-01-22 18:47:38 +0100

    readability changes

--- 2020/day21/script.js
+++ 2020/day21/script.js
@@ -1,20 +1,20 @@
 let foods, allergens = {}, knownAllergenIngredients = [];
 
 const findMatchingIngredients = (foodIds, res = []) => {
-    foodIds.map(f1id => getAvailableIngredients(foods[f1id].ingredients).forEach(i => {
+    foodIds.map(f1id => getAvailableIngredients(foods[f1id]).forEach(i => {
         if (res.includes(i)) return true;
-        if (foodIds.filter(f2id => f2id != f1id && !foods[f2id].ingredients.includes(i)).length == 0) res.push(i);
+        if (foodIds.filter(f2id => f2id != f1id && !foods[f2id].includes(i)).length == 0) res.push(i);
     }))
     return res;
 }
 
-const resolvedAllergens = () => Object.values(allergens).filter(o => o.resolved);
-const unresolvedAllergens = () => Object.values(allergens).filter(o => !o.resolved);
+const resolvedAllergens = () => Object.values(allergens).filter(o => o.resolved)
+const unresolvedAllergens = () => Object.values(allergens).filter(o => !o.resolved)
 const getAvailableIngredients = ings => ings.filter(i => resolvedAllergens().filter(o => o.value == i).length == 0)
 
-foods = input.map(l => Object({ingredients: l.split(' contains ')[0].split(' '), allergens: l.split(' contains ')[1].split(', ')}))
+foods = input.map(l => l.split(' contains ')[0].split(' '));
 
-foods.map((f, fId) => f.allergens.map(a => {
+input.map((l, fId) => l.split(' contains ')[1].split(', ').map(a => {
     if (!allergens[a]) allergens[a] = {foods: [], resolved: false, value: ''};
     allergens[a].foods.push(fId);
 }))
@@ -27,5 +27,5 @@ while (unresolvedAllergens().length > 0) unresolvedAllergens().forEach(o => {
     knownAllergenIngredients.push(found[0]);
 })
 
-console.log('part 1', foods.reduce((a, food) => a + food.ingredients.filter(i => !knownAllergenIngredients.includes(i)).length, 0));
+console.log('part 1', foods.reduce((a, f) => a + f.filter(i => !knownAllergenIngredients.includes(i)).length, 0));
 console.log('part 2', Object.keys(allergens).sort().map(k => allergens[k].value).join(','));
