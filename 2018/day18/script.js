11206e17484c34e47df272dcbc378b13d0ace11d
Jan Surovec <gh1@cloudia.cz>
2022-01-29 12:23:36 +0100

    readability changes

--- 2018/day18/script.js
+++ 2018/day18/script.js
@@ -1,40 +1,37 @@
-let states = [], cols = input[0].length, rows = input.length, TREE = '|', LUMB = '#', SPACE = '.', reps = 1000000000;
+let states = [], cols = input[0].length, rows = input.length, TREE = '|', LUMB = '#', SPACE = '.',
+    state = input.map(row => row.split('')), freq = false, reps = 1000000000;
 
-const readInput = input => input.map(row => row.split(''))
-const getCount = (map, what) => map.reduce((count, v) => count += (v == what ? 1 : 0), 0)
+const count = (arr, what) => arr.reduce((res, v) => res += (v == what ? 1 : 0), 0)
 const cmpStates = (s1, s2) => s1.every((row, y) => row.every((v, x) => v == s2[y][x]))
 
-const getAdjacent = (map, xx, yy, adj = []) => {
+const adjacent = (map, u, v, adj = []) => {
     [-1, 0, 1].forEach(i => [-1, 0, 1].forEach(j => {
-        let y = yy+i, x = xx+j;
+        let y = v+i, x = u+j;
         if (y < 0 || y >= rows || x < 0 || x >= cols || ((i == 0) && (j == 0))) return true;
         adj.push(map[y][x]);
     }))
     return adj;
 }
 
-const nextState = lastState => lastState.map((row, y) => row.map((val, x) => {
-    let adj = getAdjacent(lastState, x, y);
+const evolve = (val, adj) => {
     switch (val) {
-        case SPACE: if (getCount(adj, TREE) >= 3) val = TREE; break;
-        case TREE: if (getCount(adj, LUMB) >= 3) val = LUMB; break;
-        case LUMB: val = ((getCount(adj, TREE) >= 1) && (getCount(adj, LUMB) >= 1)) ? LUMB : SPACE; break;
+        case SPACE: if (count(adj, TREE) >= 3) val = TREE; break;
+        case TREE: if (count(adj, LUMB) >= 3) val = LUMB; break;
+        case LUMB: val = ((count(adj, TREE) >= 1) && (count(adj, LUMB) >= 1)) ? LUMB : SPACE; break;
     }
     return val;
-}));
-
-let newState = readInput(input), freq = false;
+}
 
 while (!freq) {
-    states.push(newState);
-    newState = nextState(newState);
-    states.forEach((s, i) => (cmpStates(s, newState)) && (freq = states.length - i))
+    states.push(state);
+    state = state.map((row, y) => row.map((val, x) => evolve(val, adjacent(state, x, y))))
+    states.forEach((s, i) => (cmpStates(s, state)) && (freq = states.length - i))
 }
 
 let firstDupe = states.length - freq;
 let index = reps - freq*Math.floor((reps - firstDupe)/freq);
 
-console.log('part 1', getCount(states[10].flat(), TREE)*getCount(states[10].flat(), LUMB))
-console.log('part 2', getCount(states[index].flat(), TREE)*getCount(states[index].flat(), LUMB))
+console.log('part 1', count(states[10].flat(), TREE)*count(states[10].flat(), LUMB))
+console.log('part 2', count(states[index].flat(), TREE)*count(states[index].flat(), LUMB))
 // trick for the part 2; 1 000 000 000 reps
 // from states[firstDupe] the states repeat each freq states
