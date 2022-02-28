288b908153ec56b4ac8b9252e173949af06113a2
Jan Surovec <gh1@cloudia.cz>
2022-02-28 14:34:24 +0100

    readability changes

--- 2021/day4/script.js
+++ 2021/day4/script.js
@@ -1,9 +1,7 @@
 const bingo = card => Array(5).fill(1).some((v, y) => !(card[y].reduce((a, i) => a+i, 0)*card.reduce((a, i) => a+i[y], 0)))
-const parseInput = input => {
-    const parseCardData = start => lines.slice(start, start+5).map(line => line.replace(/^\s/g, "").replace(/\s\s/g, " ").split(' ').map(n => parseInt(n)))
-    let lines = input.split("\n"), cards = [], hits = lines[0].split(',').map(n => parseInt(n));
-    for (let i = 0; i < (lines.length-1)/6; i++) cards.push(parseCardData(2+i*6));
-    return [hits, cards];
+const parseInput = (input, cards = []) => {
+    input.split("\n").filter((l, i) => l && i).map((l, i) => (cards[~~(i/5)] = cards[~~(i/5)] || []).push(l.match(/\d+/g).map(Number)))
+    return [input.split("\n")[0].split(',').map(Number), cards];
 }
 
 const turn = nr => cards = cards.filter(c => !!c).map(card => {
