a6a155243723d95bdec7122528e00407cf139807
Jan Surovec <gh1@cloudia.cz>
2022-02-28 13:51:53 +0100

    readability changes

--- 2021/day4/script.js
+++ 2021/day4/script.js
@@ -1,3 +1,4 @@
+const bingo = card => Array(5).fill(1).some((v, y) => !(card[y].reduce((a, i) => a+i, 0)*card.reduce((a, i) => a+i[y], 0)))
 const parseInput = input => {
     const parseCardData = start => lines.slice(start, start+5).map(line => line.replace(/^\s/g, "").replace(/\s\s/g, " ").split(' ').map(n => parseInt(n)))
     let lines = input.split("\n"), cards = [], hits = lines[0].split(',').map(n => parseInt(n));
@@ -5,18 +6,12 @@ const parseInput = input => {
     return [hits, cards];
 }
 
-const turn = nr => {
-    const score = card => card.flat().reduce((a, i) => a+i);
-    const checkBingo = card => Array(5).fill(1).some((v, y) => !(card[y].reduce((a, i) => a+i, 0)*card.reduce((a, i) => a+i[y], 0)))
-
-    cards = cards.filter(c => !binged.map(f => f.v).includes(c.join())).map(card => {
-        card = card.map(row => row.map(v => v == nr ? 0 : v))
-        if (checkBingo(card)) binged.push({v: card.join(), score: score(card)*nr});
-        return card;
-    })
-}
+const turn = nr => cards = cards.filter(c => !!c).map(card => {
+    card = card.map(row => row.map(v => v == nr ? 0 : v))
+    if (bingo(card)) binged.push(nr*card.flat().reduce((a,i) => a+i, 0)); else return card;
+})
 
 let [hits, cards] = parseInput(input), binged = [];
 
 hits.map(turn);
-console.log(binged[0].score, binged.pop().score);
+console.log(binged[0], binged.pop());
