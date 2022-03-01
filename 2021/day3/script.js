4e9d75a0bde06aa5b71168e6b4970f44e5f09c0c
Jan Surovec <gh1@cloudia.cz>
2022-03-01 11:56:24 +0100

    readability changes

--- 2021/day3/script.js
+++ 2021/day3/script.js
@@ -1,15 +1,14 @@
-const countFreq = (i, arr, what = '1') => arr.reduce((a, line) => a + (line[i] == what), 0)
-const invert = s => s.split('').reduce((a, n) => a + (n == '1' ? '0' : '1'), '')
-const gamma = arr => arr[0].split('').reduce((a, v, i) => a += countFreq(i, arr) < arr.length/2 ? '0' : '1')
+const onesOnPos = (i, arr) => arr.filter(l => l[i] == '1').length
+const gamma = arr => parseInt(arr[0].split('').reduce((a, v, i) => a += onesOnPos(i, arr) < arr.length/2 ? '0' : '1'), 2)
 
 const processArr = (inv = false, arr = input.slice(), bitId = 0) => {
     while (arr.length-1) {
-        let keeps = (inv ^ countFreq(bitId, arr) < arr.length/2) ? '0' : '1';
+        let keeps = (inv ^ onesOnPos(bitId, arr) < arr.length/2) ? '0' : '1';
         arr = arr.filter(line => line[bitId] == keeps);
         bitId++;
     }
-    return arr[0];
+    return parseInt(arr[0], 2);
 }
 
-console.log(parseInt(gamma(input), 2) * parseInt(invert(gamma(input)), 2));
-console.log(parseInt(processArr(), 2) * parseInt(processArr(true), 2));
+console.log(gamma(input) * (0xfff ^ gamma(input)));
+console.log(processArr() * processArr(true));
