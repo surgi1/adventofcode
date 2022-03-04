5719b8a64b0e8b862685ceae4a63bec5792913eb
Jan Surovec <gh1@cloudia.cz>
2022-03-04 15:46:02 +0100

    readability changes

--- 2021/day7/script.js
+++ 2021/day7/script.js
@@ -1,8 +1,7 @@
-const parseInput = input => input.split(",").map(n => parseInt(n));
 const alignCrabs = (progressive = false) => {
-    const energyDist = (d, res = 0) => progressive ? d*(d+1)/2 : d;
-    let crabs = parseInput(input), max = Math.max(...crabs), res = Number.MAX_SAFE_INTEGER;
-    for (let d = 1; d < max; d++) res = Math.min(res, crabs.reduce((a, c) => a+energyDist(Math.abs(c-d)), 0))
+    const dist = d => progressive ? d*(d+1)/2 : d
+    let crabs = input.split(',').map(Number), max = Math.max(...crabs), res = Number.MAX_SAFE_INTEGER;
+    while (max--) res = Math.min(res, crabs.reduce((a, c) => a+dist(Math.abs(c-max)), 0))
     return res;
 }
 
