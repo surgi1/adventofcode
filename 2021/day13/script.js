7cdc1550ff535df506c408b19920491b2f7b0853
Jan Surovec <gh1@cloudia.cz>
2022-03-04 14:59:15 +0100

    readability changes

--- 2021/day13/script.js
+++ 2021/day13/script.js
@@ -1,29 +1,24 @@
-const initScreen = (data, screen = []) => {
-    let pts = data.map(l => l.split(',').map(n => parseInt(n))),
-        dim = [0,1].map(k => Math.max(...pts.map(v => v[k]))+1);
-    screen = Array.from({length: dim[1]}, v => Array(dim[0]).fill(0));
-    pts.map(p => screen[p[1]][p[0]] = 1);
+const folds = data => data.map(l => l.split('=').map((v, k) => !k ? v : Number(v)))
+const newScreen = (cols, rows) => Array.from({length: cols}, v => Array(rows).fill(0))
+
+const initScreen = data => {
+    let ps = data.map(l => l.split(',').map(Number)),
+        screen = newScreen(Math.max(...ps.map(v => v[1]))+1, Math.max(...ps.map(v => v[0]))+1);
+    ps.map(p => screen[p[1]][p[0]] = 1);
     return screen;
 }
 
-const folds = data => data.map(l => l.split('=').map((v, k) => !k ? v : parseInt(v)))
+const fold = (prev, [axis, pos], fId, cols = prev[0].length, rows = prev.length) => {
+    const xc = (ax, v, len) => axis == ax && v > pos ? len-v-1 : v
+    let s = newScreen(axis == 'y' ? pos : rows, axis == 'x' ? pos : cols)
 
-const fold = (prev, f, fId) => {
-    let screen = Array.from({length: f[0]=='y' ? f[1] : prev.length},
-        v => Array(f[0]=='x' ? f[1] : prev[0].length).fill(0));
+    prev.forEach((r, y) => r.forEach((d, x) => d && (s[xc('y', y, rows)][xc('x', x, cols)] = 1)))
 
-    prev.forEach((row, y) => row.forEach((d, x) => {
-        if (d == 0) return;
-        let _x = f[0] == 'x' && x > f[1] ? prev[0].length-x-1 : x;
-        let _y = f[0] == 'y' && y > f[1] ? prev.length-y-1 : y;
-        screen[_y][_x] = 1;
-    }))
-
-    if (!fId) console.log(screen.flat().reduce((a, p) => a+p));
-    return screen;
+    if (!fId) console.log(s.flat().reduce((a, p) => a+p));
+    return s;
 }
 
 let screen = initScreen(screenData);
 folds(foldData).map((f, i) => screen = fold(screen, f, i));
 
-console.log(screen.map((row, y) => row.reduce((p, x) => p + (x == 1 ? '██' : '  '), '')));
+console.log(screen.map((row, y) => row.map(v => ['  ', '██'][v]).join('')).join("\n"));
