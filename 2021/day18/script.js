70b374ccd820cb142893e9669023d0bfe83e82e2
Jan Surovec <gh1@cloudia.cz>
2022-03-05 10:11:40 +0100

    readability changes

--- 2021/day18/script.js
+++ 2021/day18/script.js
@@ -1,4 +1,4 @@
-const part1 = (t = new FishTree()) => input.map(l => t.arm(l)).pop()
+const part1 = (t = new FishTree()) => input.map(t.arm).pop()
 const part2 = () => input.map((i,n) => input.filter((j,m) => n != m).map(j => new FishTree(j).arm(i)))
 
 console.log(part1());
