2360ed6da672c4e173967377316d9232eff887c2
Jan Surovec <gh1@cloudia.cz>
2022-03-05 09:40:59 +0100

    readability changes

--- 2021/day18/script.js
+++ 2021/day18/script.js
@@ -3,7 +3,5 @@ const part1 = (tree = new FishTree()) => {
     return tree.magnitude();
 }
 
-const part2 = () => Math.max(...input.map(i => input.filter(j => i != j).map(j => new FishTree(j).addArray(i).reduce().magnitude())).flat())
-
-console.log('magnitude', part1());
-console.log('largest magnitude', part2());
+console.log(part1());
+console.log(Math.max(...input.map(i => input.filter(j => i != j).map(j => new FishTree(j).addArray(i).reduce().magnitude())).flat()));
