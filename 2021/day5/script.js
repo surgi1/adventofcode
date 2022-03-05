a0ec5a3e157b7a86d1c64f9c1a27d23b57695231
Jan Surovec <gh1@cloudia.cz>
2022-03-05 15:38:29 +0100

    readability changes

--- 2021/day5/script.js
+++ 2021/day5/script.js
@@ -1,14 +1,13 @@
 const drawVents = (diagonal = false, dots = {}) => {
-    const drawPoint = (x, y) => dots['_'+x+'_'+y] = (dots['_'+x+'_'+y] || 0)+1;
-    const drawLine = (x, y, x2, y2) => {
+    const drawPoint = (x, y) => dots[x+'_'+y] = (dots[x+'_'+y] || 0)+1;
+    const drawLine = ([x, y, x2, y2]) => {
         let ix = Math.sign(x2-x), iy = Math.sign(y2-y);
         if (!diagonal && ix*iy) return;
         drawPoint(x, y);
         while (x != x2 || y != y2) drawPoint(x += ix, y += iy)
     }
 
-    input.map(l => l.split(' -> ').map(i => i.split(',').map(Number)))
-         .map(vent => drawLine(vent[0][0], vent[0][1], vent[1][0], vent[1][1]));
+    input.map(l => l.match(/\d+/g).map(Number)).map(drawLine);
     return Object.values(dots).filter(v => v > 1).length
 }
 
