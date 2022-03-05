ad15fb7023499397c97741ddbe0e825cb1bb915a
Jan Surovec <gh1@cloudia.cz>
2022-03-05 15:59:39 +0100

    readability changes

--- 2021/day9/script.js
+++ 2021/day9/script.js
@@ -1,26 +1,22 @@
-let map = input.map(l => l.split('').map(n => parseInt(n))), cols = map[0].length, rows = map.length;
-
-const adjacent = (x, y, res = []) => {
+const adjacent = (map, x, y, res = []) => {
     if (x > 0) res.push({x: x-1, y: y});
     if (y > 0) res.push({x: x, y: y-1});
-    if (x < cols-1) res.push({x: x+1, y: y});
-    if (y < rows-1) res.push({x: x, y: y+1});
+    if (x < map[0].length-1) res.push({x: x+1, y: y});
+    if (y < map.length-1) res.push({x: x, y: y+1});
     return res;
 }
 
-const part1 = (lowPoints = []) => {
-    for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) 
-        if (Math.min(...adjacent(x, y).map(p => map[p.y][p.x])) > map[y][x]) lowPoints.push({x: x, y: y})
-
-    console.log(lowPoints.reduce((acc, p) => acc+map[p.y][p.x]+1, 0));
-    return lowPoints;
+const part1 = (map, res = []) => {
+    map.map((r, y) => r.map((v, x) => Math.min(...adjacent(map, x, y).map(p => map[p.y][p.x])) > v && res.push({x: x, y: y})))
+    console.log(res.reduce((a, p) => a+map[p.y][p.x]+1, 0));
+    return [map, res];
 }
 
-const part2 = lowPoints => {
+const part2 = ([map, lowPoints]) => {
     const spread = (x, y) => {
         if (map[y][x] < 9) size++; else return;
         map[y][x] = 9;
-        adjacent(x, y).map(p => spread(p.x, p.y));
+        adjacent(map, x, y).map(p => spread(p.x, p.y));
     }
 
     let basinSizes = [], size;
@@ -30,7 +26,7 @@ const part2 = lowPoints => {
         basinSizes.push(size);
     })
 
-    return basinSizes.sort((a, b) => b-a).slice(0, 3).reduce((acc, n) => acc*n, 1);
+    return basinSizes.sort((a, b) => b-a).slice(0, 3).reduce((a, n) => a*n, 1);
 }
 
-console.log(part2(part1()));
+console.log(part2(part1(input.map(l => l.split('').map(Number)))));
