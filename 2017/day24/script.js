2036efa616c24db7a804f03e143326bfcc40c842
Jan Surovec <gh1@cloudia.cz>
2022-03-28 15:25:49 +0200

    readability changes

--- 2017/day24/script.js
+++ 2017/day24/script.js
@@ -1,63 +1,26 @@
-let components = [];
+let components = input.map(c => c.split('/').map(Number));
 
-input.map(cmp => components.push(cmp.split('/').map(n => parseInt(n))))
+let paths = components.filter(c => c.includes(0)).map(c => Object({
+    usedConnectors: [c],
+    openEnd: c[0] == 0 ? c[1] : c[1]
+}))
 
-let paths = [];
-
-components.map((cmp, id) => {
-    if (cmp.includes(0)) {
-        paths.push({
-            usedConnectorIds: [id],
-            openEnd: cmp[0] == 0 ? cmp[1] : cmp[1]
+while (paths.filter(p => !p.finished).length) {
+    paths.filter(p => !p.finished).forEach(path => {
+        components.filter((c, cId) => !path.usedConnectors.includes(c) && c.includes(path.openEnd)).forEach(c => {
+            paths.push({...path,
+                usedConnectors: [...path.usedConnectors, c],
+                openEnd: c[0] == path.openEnd ? c[1] : c[0]
+            });
         })
-    }
-})
-
-let progressed = true;
-
-while (progressed) {
-    progressed = false;
-    let len = paths.length;
-    
-    for (let pathId = 0; pathId < len; pathId++) {
-        let path = paths[pathId];
-        if (path.finished) continue;
-
-        let suitableNextComponentIds = [];
-        for (let cmpId = 0; cmpId < components.length; cmpId++) {
-            if (path.usedConnectorIds.includes(cmpId)) continue;
-            let cmp = components[cmpId];
-            if (cmp.includes(path.openEnd)) suitableNextComponentIds.push(cmpId);
-        }
-
-        if (suitableNextComponentIds.length > 0) {
-            progressed = true;
-            for (let cmpId = 0; cmpId < suitableNextComponentIds.length; cmpId++) {
-                let newPath = {...path, usedConnectorIds: path.usedConnectorIds.slice()}
-                let cmp = components[suitableNextComponentIds[cmpId]];
-                newPath.usedConnectorIds.push(suitableNextComponentIds[cmpId]);
-                newPath.openEnd = cmp[0] == newPath.openEnd ? cmp[1] : cmp[0];
-                paths.push(newPath);
-            }
-        }
         path.finished = true;
-    }
-
+    })
 }
 
-paths.map(path => {
-    let value = 0;
-    path.usedConnectorIds.map(cmpId => {
-        value += components[cmpId][0]+components[cmpId][1];
-    })
-    path.value = value;
-})
+paths.forEach(p => p.value = p.usedConnectors.reduce((a, c) => a+c[0]+c[1], 0));
 
 console.log('strongest bridge strength', paths.sort((a, b) => b.value-a.value)[0].value);
 console.log('longest bridge strength', paths.sort((a, b) => {
-    if (b.usedConnectorIds.length == a.usedConnectorIds.length) {
-        return b.value-a.value;
-    } else {
-        return b.usedConnectorIds.length-a.usedConnectorIds.length;
-    }
+    if (b.usedConnectors.length == a.usedConnectors.length) return b.value-a.value;
+    return b.usedConnectors.length-a.usedConnectors.length;
 })[0].value);
