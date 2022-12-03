869e93193055b8ecafcf62448855f9b1bf6db192
Jan Surovec <gh1@cloudia.cz>
2022-12-03 10:11:11 +0100

    day 3 minor changes

--- 2022/day3/script.js
+++ 2022/day3/script.js
@@ -1,15 +1,14 @@
-const string2Set = s => new Set(s.split(''))
-const getCode = l => l === l.toLowerCase() ? l.charCodeAt(0)-96: l.charCodeAt(0)-38;
+const getCode = c => c === c.toLowerCase() ? c.charCodeAt(0)-96: c.charCodeAt(0)-38;
 
-const part1 = () => input.split("\n").reduce((res, row) => {
-    let h1 = string2Set(row.substr(0, row.length/2)),
-        h2 = string2Set(row.substr(row.length/2));
+const part1 = () => input.split("\n").reduce((res, l) => {
+    let h1 = new Set([...l].slice(0, l.length/2)),
+        h2 = new Set([...l].slice(l.length/2));
     for (k of h1)
         if (h2.has(k)) return res + getCode(k)
 }, 0)
 
 const part2 = (res = 0) => {
-    let elves = input.split("\n").map(string2Set);
+    let elves = input.split("\n").map(s => new Set([...s]));
     for (let i = 0; i < elves.length; i += 3)
         for (k of elves[i])
             if (elves[i+1].has(k) && elves[i+2].has(k)) { res += getCode(k); break; }
