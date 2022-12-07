bc361ae46e318434eeb647de4c324c2f2ca3757c
Jan Surovec <gh1@cloudia.cz>
2022-12-07 08:42:08 +0100

    day 7 readability changes

--- 2022/day7/script.js
+++ 2022/day7/script.js
@@ -1,16 +1,14 @@
-const computeSizes = () => nodes.filter(n => n.type == 'dir').map(n => n.size = dirSize(n.id))
-const createNode = params => nodes.push({...params, id: nodes.length})-1;
 const dirSize = id => nodes[id].size || nodes.filter(n => n.parentId == id).reduce((a, n) => (n.type == 'file' ? a+n.size : a+dirSize(n.id)), 0)
+const computeSizes = () => nodes.filter(n => n.type == 'dir').map(n => n.size = dirSize(n.id))
 
-const createNodeIfNotExists = (name, parentId) => {
-    let tmp = nodes.filter(n => n.name == name && n.parentId == parentId && n.type == 'dir');
-    if (tmp.length > 0) return tmp[0].id;
-    return createNode({type: 'dir', name: name, parentId: parentId})
-}
+const processInput = (nodes = []) => {
+    const createNode = params => nodes.push({...params, id: nodes.length})-1;
+    const createNodeIfNotExists = (name, parentId) => {
+        let tmp = nodes.filter(n => n.name == name && n.parentId == parentId && n.type == 'dir');
+        return tmp.length > 0 ? tmp[0].id : createNode({type: 'dir', name: name, parentId: parentId})
+    }
 
-const processInput = () => {
-    let ptr = createNode({type: 'dir', name: ''})
-
+    let ptr = createNode({type: 'dir', name: ''});
     input.split("\n").map(line => {
         let cmd = line.split(' ');
         if (cmd[0] == '$') {
@@ -22,16 +20,14 @@ const processInput = () => {
         } else {
             if (cmd[0] == 'dir')
                 createNodeIfNotExists(cmd[0], ptr);
-            else {
+            else
                 createNode({type: 'file', name: cmd[1], size: Number(cmd[0]), parentId: ptr});
-            }
         }
     })
+    return nodes;
 }
 
-let nodes = [];
-
-processInput();
+let nodes = processInput();
 computeSizes();
 
 console.log('part 1', nodes.filter(n => n.type == 'dir' && n.size <= 100000).reduce((a, n) => a+n.size, 0));
