16ca1a443dfb587d3b3210b4a53114a74d6adff9
Jan Surovec <gh1@cloudia.cz>
2022-12-07 08:11:39 +0100

    day 7 readability changes

--- 2022/day7/script.js
+++ 2022/day7/script.js
@@ -1,10 +1,6 @@
-let nodes = [];
-
-const createNode = (params) => {
-    let newId = nodes.length;
-    nodes.push({...params, id: newId})
-    return newId;
-}
+const computeSizes = () => nodes.filter(n => n.type == 'dir').map(n => n.size = dirSize(n.id))
+const createNode = params => nodes.push({...params, id: nodes.length})-1;
+const dirSize = id => nodes.filter(n => n.parentId == id).reduce((a, n) => (n.type == 'file' ? a+n.size : a+dirSize(n.id)), 0)
 
 const createNodeIfNotExists = (name, parentId) => {
     let tmp = nodes.filter(n => n.name == name && n.parentId == parentId && n.type == 'dir');
@@ -12,48 +8,31 @@ const createNodeIfNotExists = (name, parentId) => {
     return createNode({type: 'dir', name: name, parentId: parentId})
 }
 
-const dirSize = id => nodes.filter(n => n.parentId == id).reduce((size, n) => {
-    if (n.type == 'file') return size+n.size;
-    return size+dirSize(n.id);
-}, 0)
+const processInput = () => {
+    let ptr = createNode({type: 'dir', name: ''})
 
-createNode({type: 'dir', name: ''})
-
-let ptr = 0;
-input.split("\n").map(line => {
-    let cmd = line.split(' ');
-    if (cmd[0] == '$') {
-        if (cmd[1] == 'cd') {
-            if (cmd[2] == '..') ptr = nodes[ptr].parentId || 0;
-            else if (cmd[2] == '/') ptr = 0;
-            else {
-                // cd dir
-                ptr = createNodeIfNotExists(cmd[2], ptr);
+    input.split("\n").map(line => {
+        let cmd = line.split(' ');
+        if (cmd[0] == '$') {
+            if (cmd[1] == 'cd') {
+                if (cmd[2] == '..') ptr = nodes[ptr].parentId || 0;
+                else if (cmd[2] == '/') ptr = 0;
+                else ptr = createNodeIfNotExists(cmd[2], ptr); // cd dir
             }
-        } else if (cmd[1] == 'ls') {
-            // no op
-        }
-    } else {
-        if (cmd[0] == 'dir') {
-            createNodeIfNotExists(cmd[0], ptr);
         } else {
-            // file in ptr node
-            createNode({type: 'file', name: cmd[1], size: Number(cmd[0]), parentId: ptr});
+            if (cmd[0] == 'dir')
+                createNodeIfNotExists(cmd[0], ptr);
+            else {
+                createNode({type: 'file', name: cmd[1], size: Number(cmd[0]), parentId: ptr});
+            }
         }
-    }
-})
+    })
+}
 
-let res = 0;
-nodes.forEach(n => {
-    if (n.type == 'dir') {
-        let size = dirSize(n.id);
-        if (size < 100000) res += size;
-        nodes[n.id].size = size;
-    }
-})
+let nodes = [];
 
-console.log('part 1', res);
+processInput();
+computeSizes();
 
-let freeSpace = 70000000 - nodes[0].size;
-
-console.log('part 2', nodes.filter(n => n.type == 'dir' && ((freeSpace + n.size) >= 30000000)).sort((a,b) => a.size-b.size)[0].size);
+console.log('part 1', nodes.filter(n => n.type == 'dir' && n.size <= 100000).reduce((a, n) => a+n.size, 0));
+console.log('part 2', nodes.filter(n => n.type == 'dir' && ((70000000-nodes[0].size + n.size) >= 30000000)).sort((a,b) => a.size-b.size)[0].size);
