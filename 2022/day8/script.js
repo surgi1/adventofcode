7cf72958962567b88f19001ddf4ef131a110c12d
Jan Surovec <gh1@cloudia.cz>
2022-12-08 13:59:13 +0100

    day 7 minor simplifications

--- 2022/day8/script.js
+++ 2022/day8/script.js
@@ -2,23 +2,23 @@ let map = input.split("\n").map(l => l.split('').map(Number)),
     size = map.length,
     dirs = [[0,1], [0,-1], [1,0], [-1,0]];
 
-const part1 = () => {
-    let map2 = Array.from({length: size}, () => []); 
-
+const part1 = vmap => {
     const markDir = (x, y, vx, vy, max = map[y][x]) => {
+        vmap[y][x] = 1; // mark also the starting point
         while (x >= 0 && y >= 0 && x < size && y < size) {
-            if (map[y][x] > max) map2[y][x] = 1;
+            if (map[y][x] > max) vmap[y][x] = 1;
             max = Math.max(max, map[y][x]);
             x += vx; y += vy;
         }
     }
 
-    for (let n = 1; n < size-1; n++) dirs.map(d => markDir(
-            !d[0] ? n : (d[0] == 1 ? 0 : size-1),
-            !d[1] ? n : (d[1] == 1 ? 0 : size-1), ...d
-        ))
+    // run visibility check from all the sides
+    map.forEach((r, n) => dirs.forEach(d => markDir(
+        !d[0] ? n : (d[0] == 1 ? 0 : size-1),
+        !d[1] ? n : (d[1] == 1 ? 0 : size-1), ...d
+    )))
 
-    return size*4-4 + map2.flat().length;
+    return vmap.flat().length;
 }
 
 const visDir = (x, y, vx, vy, h = map[y][x], res = 0) => {
@@ -33,5 +33,5 @@ const visDir = (x, y, vx, vy, h = map[y][x], res = 0) => {
 
 const part2 = () => Math.max(...map.map((r, y) => r.map((v, x) => dirs.reduce((a, d) => a*visDir(x,y, ...d), 1))).flat())
 
-console.log('part 1', part1());
+console.log('part 1', part1(Array.from({length: size}, () => [])));
 console.log('part 2', part2());
