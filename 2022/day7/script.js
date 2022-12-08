9ea078f51eff62ce3c464b772be5b58ad4bb77cb
Jan Surovec <gh1@cloudia.cz>
2022-12-08 19:48:12 +0100

    day 7 readability changes

--- 2022/day7/script.js
+++ 2022/day7/script.js
@@ -3,26 +3,27 @@ const computeSizes = () => nodes.filter(n => n.type == 'dir').map(n => n.size = 
 
 const processInput = (nodes = []) => {
     const createNode = params => nodes.push({...params, id: nodes.length})-1;
-    const createNodeIfNotExists = (name, parentId) => {
+    const cDir = (name, parentId) => {
         let tmp = nodes.filter(n => n.name == name && n.parentId == parentId && n.type == 'dir');
-        return tmp.length > 0 ? tmp[0].id : createNode({type: 'dir', name: name, parentId: parentId})
+        return tmp.length > 0 ? tmp[0].id : createNode({
+            type: 'dir',
+            name: name,
+            parentId: parentId
+        })
     }
 
     let ptr = createNode({type: 'dir', name: ''});
-    input.split("\n").map(line => {
+    input.split("\n").forEach(line => {
         let cmd = line.split(' ');
         if (cmd[0] == '$') {
-            if (cmd[1] == 'cd') {
-                if (cmd[2] == '..') ptr = nodes[ptr].parentId || 0;
-                else if (cmd[2] == '/') ptr = 0;
-                else ptr = createNodeIfNotExists(cmd[2], ptr); // cd dir
-            }
-        } else {
-            if (cmd[0] == 'dir')
-                createNodeIfNotExists(cmd[0], ptr);
-            else
-                createNode({type: 'file', name: cmd[1], size: Number(cmd[0]), parentId: ptr});
-        }
+            if (cmd[1] == 'cd' && cmd[2] != '/')
+                ptr = cmd[2] == '..' ? nodes[ptr].parentId || 0 : cDir(cmd[2], ptr);
+        } else if (cmd[0] !== 'dir') createNode({
+            type: 'file',
+            name: cmd[1],
+            size: Number(cmd[0]),
+            parentId: ptr
+        });
     })
     return nodes;
 }
@@ -30,5 +31,5 @@ const processInput = (nodes = []) => {
 let nodes = processInput();
 computeSizes();
 
-console.log('part 1', nodes.filter(n => n.type == 'dir' && n.size <= 100000).reduce((a, n) => a+n.size, 0));
-console.log('part 2', nodes.filter(n => n.type == 'dir' && 70000000-nodes[0].size + n.size >= 30000000).sort((a,b) => a.size-b.size)[0].size);
+console.log('part 1', nodes.filter(n => n.type == 'dir' && n.size <= 1e5).reduce((a, n) => a+n.size, 0));
+console.log('part 2', nodes.filter(n => n.type == 'dir' && 4e7 + n.size >= nodes[0].size).sort((a,b) => a.size-b.size)[0].size);
