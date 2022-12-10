2907eb1aa9c5898f25300eece385a8d274ac50f5
Jan Surovec <gh1@cloudia.cz>
2022-12-10 09:37:19 +0100

    narrowed the solution a bit

--- 2022/day10/script.js
+++ 2022/day10/script.js
@@ -1,22 +1,20 @@
-let mainCycles = [20, 60, 100, 140, 180, 220],
-    cycle = 1, x = 1, strengths = [],
+let cycle = 0, x = 1, strengths = [],
     screen = Array(6).fill().map(e => Array(40).fill(' '));
 
-const draw = (pos, char) => screen[Math.floor((pos % 240)/40)][pos % 40] = char;
+const draw = (pos, char) => screen[Math.floor(pos/40)][pos % 40] = char;
 
 const cycleOp = () => {
-    if (mainCycles.includes(cycle)) strengths.push(cycle*x);
+    cycle++;
+    if ((cycle-20) % 40 == 0) strengths.push(cycle*x);
     draw(cycle-1, Math.abs(((cycle-1) % 40) - x) <= 1 ? '#' : ' ')
 }
 
 input.split("\n").forEach(line => {
     let tmp = line.split(' ');
-    cycle++;
     cycleOp();
     if (tmp[0] == 'addx') {
-        x += Number(tmp[1]);
-        cycle++;
         cycleOp();
+        x += Number(tmp[1]);
     }
 })
 
