3540d32adc1aa455e10768870fa614b161513551
Jan Surovec <gh1@cloudia.cz>
2022-12-11 11:03:19 +0100

    minor readability changes

--- 2022/day11/script.js
+++ 2022/day11/script.js
@@ -4,7 +4,7 @@ const parseInput = (monkeys = []) => {
         tmp.stack = monLit[1].match(/\d+/g).map(Number);
         tmp.mod = Number(monLit[3].match(/\d+/));
         tmp.targets = tmp.targets.map(lineId => Number(monLit[lineId].match(/\d+/)));
-        tmp.opLit = monLit[2].split(' ').slice(-3);
+        tmp.opPars = monLit[2].split(' ').slice(-3);
         monkeys.push(tmp);
     })
     return monkeys;
@@ -12,8 +12,8 @@ const parseInput = (monkeys = []) => {
 
 const compute = (part2 = false, rounds = part2 ? 10000 : 20) => {
     const val = (v, old) => isNaN(v) ? old : Number(v);
-    const op = (old, opLit) => opLit[1] == '+' ? val(opLit[0], old)+val(opLit[2], old)
-                                               : val(opLit[0], old)*val(opLit[2], old)
+    const op = (old, pars) => pars[1] == '+' ? val(pars[0], old)+val(pars[2], old)
+                                             : val(pars[0], old)*val(pars[2], old)
 
     const round = () => {
         monkeys.forEach(monkey => {
@@ -21,9 +21,9 @@ const compute = (part2 = false, rounds = part2 ? 10000 : 20) => {
                 let item = monkey.stack.shift();
                 
                 if (!part2)
-                    item = Math.floor((op(item, monkey.opLit) % mod) / 3);
+                    item = Math.floor((op(item, monkey.opPars) % mod) / 3);
                 else
-                    item = op(item, monkey.opLit) % mod;
+                    item = op(item, monkey.opPars) % mod;
 
                 let targetId = monkey.targets[ Math.sign(item % monkey.mod) ]
                 monkeys[targetId].stack.push(item);
