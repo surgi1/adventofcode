910f21bad0ff7013beb963c0bc5eae3daaebda1c
Jan Surovec <gh1@cloudia.cz>
2022-12-11 12:13:22 +0100

    readability changes

--- 2022/day11/script.js
+++ 2022/day11/script.js
@@ -13,22 +13,15 @@ const compute = (part2 = false, rounds) => {
     const val = (v, old) => isNaN(v) ? old : Number(v);
     const op = (old, pars) => pars[1] == '+' ? val(pars[0], old)+val(pars[2], old)
                                              : val(pars[0], old)*val(pars[2], old)
-    const round = () => {
-        monkeys.forEach(monkey => {
-            while (monkey.stack.length) {
-                let item = monkey.stack.shift();
-                
-                if (!part2)
-                    item = Math.floor((op(item, monkey.opPars) % mod) / 3);
-                else
-                    item = op(item, monkey.opPars) % mod;
-
-                let targetId = monkey.targets[ Math.sign(item % monkey.mod) ]
-                monkeys[targetId].stack.push(item);
-                monkey.ins++;
-            }
-        })
-    }
+    const round = () => monkeys.forEach(monkey => {
+        while (monkey.stack.length) {
+            let item = monkey.stack.shift();
+            item = part2 ? op(item, monkey.opPars) % mod
+                         : Math.floor((op(item, monkey.opPars) % mod) / 3)
+            monkeys[monkey.targets[Math.sign(item % monkey.mod)]].stack.push(item);
+            monkey.ins++;
+        }
+    })
 
     let monkeys = parseInput();
     let mod = monkeys.reduce((a, c) => a*c.mod, 1);
