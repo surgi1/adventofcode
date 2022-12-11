87046477f0881d0a6ab409055e0c726cddc653a9
Jan Surovec <gh1@cloudia.cz>
2022-12-11 09:59:50 +0100

    minor readability changes

--- 2022/day11/script.js
+++ 2022/day11/script.js
@@ -5,40 +5,38 @@ const parseInput = (monkeys = []) => {
         tmp.mod = Number(monLit[3].match(/\d+/));
         tmp.targets = tmp.targets.map(lineId => Number(monLit[lineId].match(/\d+/)));
         tmp.opLit = monLit[2].split(' ').slice(-3);
-        monkeys.push(tmp)
+        monkeys.push(tmp);
     })
     return monkeys;
 }
 
 const compute = (part2 = false, rounds = part2 ? 10000 : 20) => {
     const val = (v, old) => isNaN(v) ? old : Number(v);
-    const op = (old, opLit) => opLit[1] == '+' ? val(opLit[0], old)+val(opLit[2], old) : val(opLit[0], old)*val(opLit[2], old)
+    const op = (old, opLit) => opLit[1] == '+' ? val(opLit[0], old)+val(opLit[2], old)
+                                               : val(opLit[0], old)*val(opLit[2], old)
 
     const round = () => {
-        for (let i = 0; i < monkeys.length; i++) {
-            while (monkeys[i].stack.length) {
-                let item = monkeys[i].stack.shift();
+        monkeys.forEach(monkey => {
+            while (monkey.stack.length) {
+                let item = monkey.stack.shift();
                 
-                if (!part2) {
-                    item = Math.floor((op(item, monkeys[i].opLit) % mod) / 3); // p1
-                } else {
-                    item = op(item, monkeys[i].opLit) % mod; // p2
-                }
+                if (!part2)
+                    item = Math.floor((op(item, monkey.opLit) % mod) / 3);
+                else
+                    item = op(item, monkey.opLit) % mod;
 
-                let targetId = monkeys[i].targets[ Math.sign(item % monkeys[i].mod) ]
+                let targetId = monkey.targets[ Math.sign(item % monkey.mod) ]
                 monkeys[targetId].stack.push(item);
-                monkeys[i].ins++;
+                monkey.ins++;
             }
-        }
+        })
     }
 
     let monkeys = parseInput();
     let mod = monkeys.reduce((a, c) => a*c.mod, 1);
 
     while (rounds--) round();
-
     return monkeys.sort((a,b) => b.ins-a.ins).slice(0,2).reduce((a,c) => a*c.ins, 1);
-
 }
 
 console.log(compute());
