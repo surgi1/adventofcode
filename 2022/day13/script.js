7c924ca3bd1e89434faff7e2f02dab2ff1ac7960
Jan Surovec <gh1@cloudia.cz>
2022-12-13 10:03:09 +0100

    day 13 proper array comparison fnc

--- 2022/day13/script.js
+++ 2022/day13/script.js
@@ -1,13 +1,10 @@
 const arrayCmp = (a, b) => {
-    if (!Array.isArray(a) && !Array.isArray(b)) {
-        if (a == b) return;
-        return a < b;
-    }
+    if (!Array.isArray(a) && !Array.isArray(b)) return Math.sign(a - b);
 
     if (Array.isArray(a) && Array.isArray(b)) {
         for (let i = 0; i < Math.min(a.length, b.length); i++) {
-            let subRes = arrayCmp(a[i], b[i])
-            if (subRes != null) return subRes;
+            let subRes = arrayCmp(a[i], b[i]);
+            if (subRes != 0) return subRes;
         }
         return arrayCmp(a.length, b.length);
     }
@@ -22,11 +19,11 @@ const part1 = () => input.split("\n\n").reduce((res, rp, id) => {
     let tmp = rp.split("\n");
     let a = JSON.parse(tmp[0]), b = JSON.parse(tmp[1]);
     items.push(a, b);
-    return res + (arrayCmp(a, b) ? id+1 : 0);
+    return res + (arrayCmp(a, b) == -1 ? id+1 : 0);
 }, 0)
 
 const part2 = extraItems => [...items, ...extraItems]
-        .sort((a, b) => arrayCmp(b, a) - arrayCmp(a, b))
+        .sort(arrayCmp)
         .reduce((a, v, i) => (extraItems.includes(v) ? a*(i+1) : a), 1)
 
 
