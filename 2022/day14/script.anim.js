b3c2f9eb1a83fd82e3b15f8a029c7ccb3e73f323
Jan Surovec <gh1@cloudia.cz>
2022-12-14 13:11:12 +0100

    day 14 animation is now significantly less greedy

--- 2022/day14/script.anim.js
+++ 2022/day14/script.anim.js
@@ -1,6 +1,6 @@
-const canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d"); sandStart = [500,0];
+const canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d"), sandStart = [500,0];
 const scale = 4, shift = 300;
-let screen = [], sand = [], ticks = 0;
+let screen = [], sand = [], ticks = 0, octx, drawing = false;
 
 let caves = input.split("\n").map(row => row.split(' -> ').map(p => p.split(',').map(Number))),
     maxX = Math.max(...caves.flat().map(v => v[0]))*2,
@@ -14,67 +14,77 @@ const init = () => {
             max = [ Math.max(from[0], to[0]), Math.max(from[1], to[1]) ];
         for (let i = min[0]; i <= max[0]; i++) for (let j = min[1]; j <= max[1]; j++) screen[j][i] = 1;
     }
-
     screen = Array.from({length: maxY+3}, () => Array(maxX).fill(0));
     caves.forEach(point => point.forEach((p, i) => i && line(point[i-1], p)))
 }
 
-const advanceSand = () => {
-    sand.filter(p => p.state == 'moving').forEach(p => {
-        let moved = false;
-        if (screen[p.pos[1]+1][p.pos[0]] == 0) { p.pos[1]++; moved = true }
-        if (screen[p.pos[1]+1][p.pos[0]-1] == 0) { p.pos[1]++; p.pos[0]--; moved = true }
-        if (screen[p.pos[1]+1][p.pos[0]+1] == 0) { p.pos[1]++; p.pos[0]++; moved = true }
-        if (!moved) {
-            screen[p.pos[1]][p.pos[0]] = 2;
-            p.state = 'solid'
-        }
-    })
-}
+const advanceSand = () => sand.filter(p => p.state == 'moving').forEach(p => {
+    let moved = false;
+    if (screen[p.pos[1]+1][p.pos[0]] == 0) { p.pos[1]++; moved = true }
+    if (screen[p.pos[1]+1][p.pos[0]-1] == 0) { p.pos[1]++; p.pos[0]--; moved = true }
+    if (screen[p.pos[1]+1][p.pos[0]+1] == 0) { p.pos[1]++; p.pos[0]++; moved = true }
+    if (!moved) {
+        screen[p.pos[1]][p.pos[0]] = 2;
+        p.state = 'solid'
+    }
+})
 
 const dropSand = () => sand.push({pos: sandStart.slice(), state: 'moving', drawn: false})
 
 const initBackground = () => {
     const drawLine = (from, to) => {
-        ctx.moveTo((from[0]-shift)*scale+0.0, from[1]*scale);
-        ctx.lineTo((to[0]-shift)*scale+0.0, to[1]*scale);
+        octx.moveTo((from[0]-shift)*scale, from[1]*scale);
+        octx.lineTo((to[0]-shift)*scale, to[1]*scale);
     }
 
     canvas.offscreenCanvas = document.createElement("canvas");
     canvas.offscreenCanvas.width = canvas.width;
     canvas.offscreenCanvas.height = canvas.height;
 
-    let ctx = canvas.offscreenCanvas.getContext("2d")
+    octx = canvas.offscreenCanvas.getContext("2d")
     
-    ctx.lineWidth = 2;
+    octx.lineWidth = 2;
     caves.forEach(point => point.forEach((p, i) => i && drawLine(point[i-1], p)))
-    ctx.stroke();
+    octx.stroke();
 
     // draw sand start point
-    ctx.fillStyle = '#E1AD9D';
+    octx.fillStyle = '#f1b96F';
+    octx.beginPath();
+    octx.arc((sandStart[0]-shift)*scale, sandStart[1]*scale, scale*3, 0, 2 * Math.PI);
+    octx.fill();
+}
+
+const drawGrain = (ctx, grain) => {
+    ctx.fillStyle = grain.state == 'solid' ? '#F4A460' : '#ffc98F';
     ctx.beginPath();
-    ctx.arc(Math.round((sandStart[0]-shift)*scale), Math.round(sandStart[1]*scale),scale*3, 0, 2 * Math.PI);
+    ctx.arc((grain.pos[0]-shift)*scale, grain.pos[1]*scale, scale/2, 0, 2 * Math.PI);
     ctx.fill();
 }
 
 const draw = () => {
+    if (drawing) return;
+    drawing = true;
+    sand.filter(g => !g.drawn && g.state == 'solid').map(g => {
+        drawGrain(octx, g);
+        g.drawn = true;
+    })
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(canvas.offscreenCanvas, 0, 0);
-    sand.forEach(p => {
-        let targetCtx = ctx;
-        ctx.fillStyle = p.state == 'solid' ? '#F4A460' : '#E1A95F';
-        ctx.fillRect(Math.round((p.pos[0]-shift)*scale-scale/2), (p.pos[1]*scale-scale/16), scale, scale);
-    })
+
+    sand.filter(g => !g.drawn).map(g => drawGrain(ctx, g))
+    drawing = false;
 }
 
 const tick = () => {
     if (ticks % 2 == 0) dropSand();
     advanceSand();
-    if (ticks % 3 == 0) draw();
+    draw();
     ticks++;
+    setTimeout(tick, 1);
 }
 
 init();
 initBackground();
 
-setInterval(tick, 5);
+tick();
