c5ccb8217f482f1177f595630741156b232738d7
Jan Surovec <gh1@cloudia.cz>
2022-12-21 10:05:18 +0100

    readability changes

--- 2022/day21/script.js
+++ 2022/day21/script.js
@@ -1,16 +1,16 @@
-let monkeys = {}, notConst = new Set();
+let monkeys = {}, notConst = new Set(['root']);
 
 input.split("\n").map(line => {
     let tmp = line.split(' ');
-    let k = tmp[0].substr(0,4);
-    if (tmp.length == 2) monkeys[k] = Number(tmp[1])
-        else monkeys[k] = tmp.slice(-3);
+    monkeys[tmp[0].substr(0,4)] = tmp.length == 2 ? Number(tmp[1]) : tmp.slice(-3);
 })
 
 const compute = (m, monkeys, part2 = true, res = 0) => {
     if (!isNaN(monkeys[m])) return monkeys[m];
 
-    if (m == 'root' && part2) res = compute(monkeys[m][0], monkeys) == compute(monkeys[m][2], monkeys); else {
+    if (m == 'root' && part2) {
+        res = compute(monkeys[m][0], monkeys) == compute(monkeys[m][2], monkeys);
+    } else {
         if (monkeys[m][1] == '+') res = compute(monkeys[m][0], monkeys)+compute(monkeys[m][2], monkeys);
         if (monkeys[m][1] == '-') res = compute(monkeys[m][0], monkeys)-compute(monkeys[m][2], monkeys);
         if (monkeys[m][1] == '*') res = compute(monkeys[m][0], monkeys)*compute(monkeys[m][2], monkeys);
@@ -23,30 +23,27 @@ const compute = (m, monkeys, part2 = true, res = 0) => {
 
 const parenthe = v => v.match(/[\+\-\*\\=]/) ? '('+v+')' : v;
 
-const print = (m, monkeys, res = '') => {
+const print = (m, monkeys) => {
     if (m == 'humn') return 'x'; // so wolfram alpha catches up
     if (notConst.has(m) && m != 'root' && !isNaN(monkeys[m])) return m;
 
     if (!isNaN(monkeys[m])) return monkeys[m]+'';
 
-    if (m == 'root') res = parenthe(print(monkeys[m][0], monkeys))+'='+parenthe(print(monkeys[m][2], monkeys)); else {
-        if (monkeys[m][1] == '+') res = parenthe(print(monkeys[m][0], monkeys))+'+'+parenthe(print(monkeys[m][2], monkeys));
-        if (monkeys[m][1] == '-') res = parenthe(print(monkeys[m][0], monkeys))+'-'+parenthe(print(monkeys[m][2], monkeys));
-        if (monkeys[m][1] == '*') res = parenthe(print(monkeys[m][0], monkeys))+'*'+parenthe(print(monkeys[m][2], monkeys));
-        if (monkeys[m][1] == '/') res = parenthe(print(monkeys[m][0], monkeys))+'/'+parenthe(print(monkeys[m][2], monkeys));
-    }
-    return res;
+    if (m == 'root') return parenthe(print(monkeys[m][0], monkeys))+'='+parenthe(print(monkeys[m][2], monkeys));
+    if (monkeys[m][1] == '+') return parenthe(print(monkeys[m][0], monkeys))+'+'+parenthe(print(monkeys[m][2], monkeys));
+    if (monkeys[m][1] == '-') return parenthe(print(monkeys[m][0], monkeys))+'-'+parenthe(print(monkeys[m][2], monkeys));
+    if (monkeys[m][1] == '*') return parenthe(print(monkeys[m][0], monkeys))+'*'+parenthe(print(monkeys[m][2], monkeys));
+    if (monkeys[m][1] == '/') return parenthe(print(monkeys[m][0], monkeys))+'/'+parenthe(print(monkeys[m][2], monkeys));
 }
 
 const findNotConst = monkeys => {
     let m1 = {...monkeys}, m2 = {...monkeys}
 
     compute('root', m1);
-    m2.humn++;
+    m2.humn += 139; // random number, we just need the humn to change
     compute('root', m2);
 
     Object.keys(m1).filter(k => m1[k] != m2[k]).map(k => notConst.add(k));
-    notConst.add('root');
 }
 
 const part2 = monkeys => {
