d62774d7bdde9ecd38e54fe40f321819a8fb3b18
Jan Surovec <gh1@cloudia.cz>
2022-12-24 16:03:36 +0100

    comments

--- 2022/day24/script.js
+++ 2022/day24/script.js
@@ -1,5 +1,5 @@
-// the trick: do not compute blizzard map for each minute repeatedly
-// then it is simple bfs with tracing visited triplets of x, y and time
+// the speed-up: do not compute blizzard map for each minute repeatedly
+// simple bfs with tracing visited triplets of x, y and time
 
 let map = [], blizMaps = [];
 
@@ -18,26 +18,28 @@ const getBlizMap = t => {
     let h = map.length-2, w = map[0].length-2;
     let bmap = map.map(l => l.map(v => v == '#' ? 1 : 0 ));
 
-    map.map((l, y) => l.map((v, x) => {
+    map.forEach((l, y) => l.forEach((v, x) => {
         if (v == '^') bmap[safeMod(y-1-t, h) + 1][x] = 1;
         if (v == 'v') bmap[((y-1+t) % h) + 1][x] = 1;
         if (v == '>') bmap[y][((x-1+t) % w) + 1] = 1;
         if (v == '<') bmap[y][safeMod(x-1-t, w) + 1] = 1;
     }))
 
-    blizMaps[t] = bmap;
-    return bmap;
+    return blizMaps[t] = bmap;
 }
 
 const run = (start, end, t0) => {
-    const k = p => p.x+'_'+p.y+'_'+ p.t;
+    const key = p => p.x+'_'+p.y+'_'+ p.t;
 
     let paths = [{x: start.x, y: start.y, t: t0}], seen = new Set();
     while (paths.length) {
-        let p = paths.shift();
-        if (seen.has(k(p))) continue;
-        seen.add(k(p));
+        let p = paths.shift(), k = key(p);
+        
+        if (seen.has(k)) continue;
+        seen.add(k);
+        
         if (p.y == end.y && p.x == end.x) return p.t;
+        
         let bmap = getBlizMap(p.t+1);
         dirs.forEach(([dx, dy]) => bmap[p.y+dy] && bmap[p.y+dy][p.x+dx] == 0 && paths.push({y: p.y+dy, x: p.x+dx, t: p.t+1}))
     }
