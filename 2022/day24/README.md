07b8e22aa7201a51da315904976a91a6c176ab58
Jan Surovec <gh1@cloudia.cz>
2022-12-24 11:25:46 +0100

    day 24

---
+++ 2022/day24/README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2022 - Day 24
+
+https://adventofcode.com/2022/day/24
+
+JavaScript implementation
+
+Check it live at https://surgi1.github.io/adventofcode/2022/day24/index.html
---
+++ 2022/day24/script.js
@@ -0,0 +1,71 @@
+// the trick: the whole map state will repeat itself after lcm(h-2, w-2)
+// let's precompute all those states (blizMaps)
+// then it is simple bfs with tracing visited triplets of x, y and time
+
+let map = [], blizMaps = [], t;
+
+const gcd = (a, b) => a ? gcd(b % a, a) : b;
+const lcm = (a, b) => a * b / gcd(a, b);
+
+const negMod = (a, b) => {
+  a += b*100000000;
+  return a % b;
+}
+
+const dirs = [
+  [-1, 0],
+  [0, -1],
+  [0, 0],
+  [1, 0],
+  [0, 1]
+];
+
+const init = input => {
+    map = input.split("\n").map(l => l.split(''))
+
+    let blizMapH = map.length-2,
+        blizMapW = map[0].length-2;
+    
+    blizMaps = Array.from({length: lcm(blizMapH, blizMapW)}, () => map.map(l => l.map(v => v == '#' ? 1 : 0 )) )
+
+    map.map((l, y) => l.map((v, x) => blizMaps.map((bmap, t) => {
+        if (v == '^') bmap[negMod(y-1-t, blizMapH) + 1][x] = 1;
+        if (v == 'v') bmap[((y-1+t) % blizMapH) + 1][x] = 1;
+        if (v == '>') bmap[y][((x-1+t) % blizMapW) + 1] = 1;
+        if (v == '<') bmap[y][negMod(x-1-t, blizMapW) + 1] = 1;
+    })))
+}
+
+const run = (start, end, t0) => {
+    const k = p => p.x+'_'+p.y+'_'+ p.t % blizMaps.length;
+
+    let paths = [{x: start.x, y: start.y, t: t0}];
+    let visited = new Set();
+
+    while (paths.length) {
+        let p = paths.shift();
+
+        if (visited.has(k(p))) continue;
+        visited.add(k(p));
+
+        if (p.y == end.y && p.x == end.x) return p.t;
+
+        dirs.forEach(([dy, dx]) => {
+            if (blizMaps[(p.t+1) % blizMaps.length][p.y+dy])
+                if (blizMaps[(p.t+1) % blizMaps.length][p.y+dy][p.x+dx] == 0) 
+                    paths.push({y: p.y+dy, x: p.x+dx, t: p.t+1})
+        })
+    }
+}
+
+init(input);
+
+let start = {x: map[0].indexOf('.'), y: 0};
+let end = {x: map[map.length-1].indexOf('.'), y: map.length-1};
+
+t = run(start, end, 0);
+console.log('part 1', t);
+
+t = run(end, start, t);
+t = run(start, end, t);
+console.log('part 2', t);
