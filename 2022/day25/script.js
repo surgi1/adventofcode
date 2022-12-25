356bb3ac09eb3b1c59fe1c7be2c2e248d665cd0c
Jan Surovec <gh1@cloudia.cz>
2022-12-25 12:07:50 +0100

    minor tweaks

--- 2022/day25/script.js
+++ 2022/day25/script.js
@@ -3,14 +3,13 @@ const snafuVal = d => Object.entries(snafuVals).filter(([k, v]) => v == d)[0][0]
 const snafu2dec = snafu => snafu.split('').reverse().reduce((a, v, i) => a+Math.pow(5, i)*snafuVals[v], 0)
 const maxSnafu = i => snafu2dec('2'.repeat(i+1))
 
-const dec2snafu = (dec, snafu = '') => {
-    let i = 0, lastI;
+const dec2snafu = (dec, i, j, snafu = '') => {
     while (dec != 0) {
-        lastI = i;
+        j = i;
         i = 0;
         while (maxSnafu(i) < Math.abs(dec)) i++;
         let inc = Math.round(dec/Math.pow(5, i));
-        snafu += '0'.repeat(Math.max(0, lastI-(i+1))) + snafuVal(inc);
+        snafu += '0'.repeat(Math.max(0, j-i-1)) + snafuVal(inc);
         dec -= inc*Math.pow(5, i)
     }
     return snafu+'0'.repeat(i);
