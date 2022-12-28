5f731b5215794e62efbd0509c8fd9ee627c0e278
Jan Surovec <gh1@cloudia.cz>
2022-12-28 23:30:45 +0100

    rewrote the solution to much simpler bfs

--- 2016/day11/script.js
+++ 2016/day11/script.js
@@ -1,15 +1,7 @@
-// just part 2
-
 const minFloor = 1, maxFloor = 4;
-let baseState = [];
-
-baseState.push(
+let baseState = [
     {type: 'generator', element: 'promethium', floor: 1},
     {type: 'microchip', element: 'promethium', floor: 1},
-    {type: 'generator', element: 'elerium', floor: 1},
-    {type: 'microchip', element: 'elerium', floor: 1},
-    {type: 'generator', element: 'dilithium', floor: 1},
-    {type: 'microchip', element: 'dilithium', floor: 1},
     {type: 'generator', element: 'cobalt', floor: 2},
     {type: 'generator', element: 'curium', floor: 2},
     {type: 'generator', element: 'ruthenium', floor: 2},
@@ -19,145 +11,83 @@ baseState.push(
     {type: 'microchip', element: 'ruthenium', floor: 3},
     {type: 'microchip', element: 'plutonium', floor: 3},
     {type: 'elevator', floor: 1}
-)
-baseState.map((item, id) => item.id = id);
+]
+baseState.forEach((item, id) => item.id = id);
 
-const cloneState = state => state.map(e => Object({...e}))
-const getGeneratorFor = (element, state) => state.filter(item => item.type == 'generator' && item.element == element)[0]
+const cloneState = state => state.map(e => ({...e}))
+const generatorFor = (element, state) => state.filter(i => i.type == 'generator' && i.element == element)[0]
+const generators = (state, floor) => state.filter(i => i.type == 'generator' && (floor === undefined ? true : i.floor == floor) );
+const chips = (state, shielded) => state.filter(i => i.type == 'microchip' && (shielded === undefined ? true : i.shielded === shielded) );
+const elevator = state => state.filter(i => i.type == 'elevator')[0];
+const itemsOnFloor = (state, floor) => state.filter(i => i.type != 'elevator' && i.floor == floor);
+const allOnTop = state => state.every(i => i.floor == maxFloor)
+const byFloor = (a, b) => a.floor - b.floor;
 
 const validate = state => {
-    let valid = true;
     // power-up microchip shields first
-    state.filter(item => item.type == 'microchip').map(microchip => {
-        microchip.shielded = (getGeneratorFor(microchip.element, state).floor == microchip.floor);
+    chips(state).forEach(m => {
+        m.shielded = (generatorFor(m.element, state).floor == m.floor);
     })
 
-    state.filter(item => item.type == 'microchip' && !item.shielded).some(microchip => {
-        if (state.filter(item => item.type == 'generator' && item.floor == microchip.floor).length > 0) {
-            valid = false;
-            return true;
-        }
-    })
-    return valid;
+    return chips(state, false).every(m => generators(state, m.floor).length == 0)
 }
 
-const footprint = state => {
-    let fp = '';
-    state.map(item => fp += item.floor);
-    return parseInt(fp);
-}
-
-const getItemCombinations = (availableItemIds, includeDuos = true) => {
-    let itemCombinations = [];
-    availableItemIds.map(itemId => itemCombinations.push([itemId]));
-    if (!includeDuos) return itemCombinations;
-    for (let i = 0; i < availableItemIds.length; i++) {
-        for (let j = i; j < availableItemIds.length; j++) {
-            if (i != j) itemCombinations.push([availableItemIds[i], availableItemIds[j]]);
-        }
-    }
+const getItemCombinations = availableItemIds => {
+    let itemCombinations = availableItemIds.map(i => [i]);
+    for (let i = 0; i < availableItemIds.length; i++)
+        for (let j = i+1; j < availableItemIds.length; j++)
+            itemCombinations.push([availableItemIds[i], availableItemIds[j]]);
     return itemCombinations;
 }
 
-const constructNextStates = (state, floor, movedItemIdsArr) => {
-    let nextStates = [];
-    movedItemIdsArr.map(idsToMove => {
-        let nextState = cloneState(state);
-        nextState.filter(i => i.type == 'elevator')[0].floor = floor;
-        idsToMove.map(idToMove => nextState[idToMove].floor = floor);
-        nextStates.push(nextState);
-    })
-    return nextStates.filter(st => validate(st));
-}
+const constructNextStates = (state, floor, combs) => combs.map(idsToMove => {
+    let nextState = cloneState(state);
+    elevator(nextState).floor = floor;
+    idsToMove.forEach(id => nextState[id].floor = floor);
+    return nextState;
+}).filter(s => validate(s))
 
-const nextStates = (state, significantStateMode) => {
-    let nextStates = [];
-    let elevator = state.filter(item => item.type == 'elevator')[0];
+const nextStates = state => {
+    let nextStates = [], elFloor = elevator(state).floor,
+        itemCombination = getItemCombinations(itemsOnFloor(state, elFloor).map(i => i.id));
 
-    let availableItemIds = [];
-    state.filter(item => item.type != 'elevator' && item.floor == elevator.floor).map(item => availableItemIds.push(item.id));
-    let itemCombinationsUp = getItemCombinations(availableItemIds);
-    let itemCombinationsDown;
+    if (elFloor < maxFloor) nextStates.push(...constructNextStates(state, elFloor+1, itemCombination));
+    if (elFloor > minFloor) nextStates.push(...constructNextStates(state, elFloor-1, itemCombination));
 
-    if (significantStateMode == 'all') {
-        availableItemIds = [];
-        state.filter(item => item.type == 'microchip' && item.floor == elevator.floor).map(item => availableItemIds.push(item.id));
-        itemCombinationsDown = getItemCombinations(availableItemIds, false);
-    } else {
-        itemCombinationsDown = itemCombinationsUp;
-    }
-
-    if (elevator.floor < maxFloor) {
-        nextStates.push(...constructNextStates(state, elevator.floor+1, itemCombinationsUp));
-    }
-    if (elevator.floor > minFloor) {
-        nextStates.push(...constructNextStates(state, elevator.floor-1, itemCombinationsDown));
-    }
     return nextStates;
 }
 
-const significantState = (state, floor, count, mode) => {
-    let valid = true;
-    if (mode == 'generators') {
-        let generatorsOnTop = state.filter(item => item.type == 'generator' && item.floor >= floor).length;
-        if (generatorsOnTop < count) valid = false;
-    } else {
-        let itemsOnTop = state.filter(item => item.floor >= maxFloor).length;
-        if (itemsOnTop < state.length) valid = false;
+// this is the trick: state reduction; as lots of them are in fact the same
+const footprint = state => elevator(state).floor
+        + generators(state).sort(byFloor).map(i => i.floor).join('_')
+        + chips(state).sort(byFloor).map(i => i.floor).join('_');
+
+const run = () => {
+    let statesReached = new Set(),
+        paths = [{ticks: 0, state: cloneState(baseState)}];
+
+    while (true) {
+        let path = paths.shift();
+        let k = footprint(path.state);
+        if (statesReached.has(k)) continue;
+        statesReached.add(k);
+
+        if (allOnTop(path.state)) return path;
+
+        nextStates(path.state).forEach(state => paths.push({ticks: path.ticks+1, state: state}))
     }
-    return valid;
 }
 
-let statesReached = [],
-    stop = false,
-    paths = [{ticks: 0, state: cloneState(baseState)}],
-    pathsIterFrom = 0, pathsIterTo = paths.length,
-    endingPath = false,
-    floorToFill = maxFloor-1,
-    generatorsToFill = 1,
-    significantStateMode = 'generators';
-
-while (true) {
-
-    while (!stop && paths.length > pathsIterFrom) {
-        pathsIterTo = paths.length;
-        for (let pathId = pathsIterFrom; pathId < pathsIterTo; pathId++) {
-            let path = paths[pathId];
-            let fp = footprint(path.state);
-            if (statesReached[fp] !== undefined && statesReached[fp] < path.ticks) {
-                continue;
-            } else {
-                statesReached[fp] = path.ticks;
-            }
-            if (significantState(path.state, floorToFill, generatorsToFill, significantStateMode)) {
-                if (significantStateMode != 'all') console.log('Significant state reached', path, 'with', generatorsToFill, 'generators on the floor nr.', floorToFill);
-                endingPath = {ticks: path.ticks, state: cloneState(path.state)}
-                stop = true;
-                break;
-            }
-            nextStates(path.state, significantStateMode).map(ns => paths.push({ticks: path.ticks+1, state: ns}))
-        }
-        pathsIterFrom = pathsIterTo;
-    }
-
-    if (significantStateMode == 'all') {
-        console.log('All items are on the max floor', endingPath, '! The job took', endingPath.ticks, 'elevator rides.');
-        break;
-    }
-
-    paths = [endingPath];
-    pathsIterFrom = 0;
-    stop = false;
-
-    generatorsToFill++;
-    if (generatorsToFill > baseState.filter(item => item.type == 'generator').length) {
-        generatorsToFill = 1;
-        floorToFill++;
-    }
-
-    if (floorToFill > maxFloor) {
-        console.log('All generators are on the max floor', endingPath, ', moving the microchips now...');
-        significantStateMode = 'all';
-    }
-
+const part2 = () => {
+    baseState.push(
+        {type: 'generator', element: 'elerium', floor: 1},
+        {type: 'microchip', element: 'elerium', floor: 1},
+        {type: 'generator', element: 'dilithium', floor: 1},
+        {type: 'microchip', element: 'dilithium', floor: 1}
+    )
+    baseState.forEach((item, id) => item.id = id);
+    return run();
 }
+
+console.log('part 1', run().ticks);
+console.log('part 2', part2().ticks);
