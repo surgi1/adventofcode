5bf58871c3c371ba4835996558e24f5d52c5fa28
Jan Surovec <gh1@cloudia.cz>
2022-12-28 11:52:32 +0100

    minor code reorg

--- 2022/day24/script.game.js
+++ 2022/day24/script.game.js
@@ -30,9 +30,8 @@ const keyMap = {
 
 let map = [], blizMaps = [], dirs = Object.values(moves), movesEntries = Object.entries(moves),
     autoRun = false, elf = {}, step = 0, steps, start, end,
-    canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d"), spriteSize = 32,
-    drawing = false, sprites = new Image(),
-    frame = 0, stepStartFrame = false, keysPressed = {};
+    canvas = document.getElementById('canvas'), ctx = canvas.getContext('2d'), spriteSize = 32,
+    drawing = false, sprites = new Image(), frame = 0, stepStartFrame = false, keysPressed = {};
 
 const getBlizMap = t => {
     const safeMod = (a, b) => (a+b*100000000) % b;
@@ -52,7 +51,7 @@ const getBlizMap = t => {
     return blizMaps[t] = bmap;
 }
 
-const run = (start, end, t0) => {
+const findBestPath = (start, end, t0) => {
     const key = p => p.x+'_'+p.y+'_'+ p.t;
 
     let paths = [{x: start.x, y: start.y, t: t0, steps: []}], seen = new Set();
@@ -75,22 +74,22 @@ const run = (start, end, t0) => {
     }
 }
 
+const createPlane = src => {
+    let e = document.createElement('canvas');
+    e.width = src.width;
+    e.height = src.height;
+    return e;
+}
+
 const initBackground = () => {
-    canvas.offscreenCanvas = document.createElement("canvas");
-    canvas.offscreenCanvas.width = canvas.width;
-    canvas.offscreenCanvas.height = canvas.height;
-
-    canvas.offscreenCanvas2 = document.createElement("canvas");
-    canvas.offscreenCanvas2.width = canvas.width;
-    canvas.offscreenCanvas2.height = canvas.height;
-
-    let octx = canvas.offscreenCanvas.getContext("2d");
-    let octx2 = canvas.offscreenCanvas2.getContext("2d");
+    canvas.planes = {
+        walls: createPlane(canvas),
+        ground: createPlane(canvas)
+    };
     
     for (let y = 0; y < map.length; y++) for (let x = 0; x < map[0].length; x++) {
-        let v = map[y][x], target = octx;
-        let bgColor = v == '#' ? wallColors[Math.floor(Math.random()*wallColors.length)] : grndColors[Math.floor(Math.random()*grndColors.length)];
-        if (v == '#') target = octx2;
+        let v = map[y][x], target = canvas.planes[v == '#' ? 'walls' : 'ground'].getContext('2d'),
+            bgColor = v == '#' ? wallColors[Math.floor(Math.random()*wallColors.length)] : grndColors[Math.floor(Math.random()*grndColors.length)];
         target.beginPath();
         target.fillStyle = bgColor;
         target.rect(x*32, y*32, 32, 32);
@@ -100,91 +99,89 @@ const initBackground = () => {
     setInterval(draw, 10);
 }
 
-const adjust = (dir, anim, v) => moves[v][dir]*anim
-const drawSprite = (spriteId, x, y, ax, ay) => ctx.drawImage(sprites, spriteId*spriteSize, 0, spriteSize, spriteSize, x*spriteSize+ax, y*spriteSize+ay, spriteSize, spriteSize);
+const advanceBlizzards = blizs => blizs.map(b => {
+    b.x += moves[b.t][0];
+    b.y += moves[b.t][1];
+    if (b.y < 1) b.y = map.length-2;
+    if (b.x < 1) b.x = map[0].length-2;
+    if (b.y > map.length-2) b.y = 1;
+    if (b.x > map[0].length-2) b.x = 1;
+    return b;
+})
+
+const adjust = (dir, animFrame, v) => moves[v][dir]*animFrame
+const drawSprite = (spriteId, [x, y], [ax, ay]) => ctx.drawImage(sprites, spriteId*spriteSize, 0, spriteSize, spriteSize, x*spriteSize+ax, y*spriteSize+ay, spriteSize, spriteSize);
+
+const drawElf = animFrame => {
+    let elfAnimAdj = [0,1].map(d => adjust(d, animFrame, elf.action));
+    drawSprite(elf.spriteId, [elf.x, elf.y], elfAnimAdj);
+    
+    // scroll page into elf view, update hp
+    const params = {behavior: 'smooth', inline: 'center', block: 'center'};
+    let el = document.getElementById('elf');
+    el.style.left = (elf.x*spriteSize - 112 + elfAnimAdj[0])+'px';
+    el.style.top = (elf.y*spriteSize - 64 + elfAnimAdj[1])+'px';
+    el.innerHTML = '❤'+elf.hp;//❤★
+    if (el.scrollIntoViewIfNeeded) el.scrollIntoViewIfNeeded(params); else el.scrollIntoView(params);
+}
+
+const drawBlizzards = animFrame => {
+    //if (animFrame > 0) { ctx.save(); ctx.globalAlpha = 0.5+Math.abs(animFrame-16)/32;}
+    blizs.forEach(b => {
+        let spriteId = 3+'<^v>'.indexOf(b.t), animAdj = [0,1].map(d => adjust(d, animFrame, b.t));
+        drawSprite(spriteId, [b.x, b.y], animAdj);
+        
+        // extra winds for the border transitions
+        if (b.y*spriteSize+animAdj[1] < 32) drawSprite(spriteId, [b.x, map.length-1], animAdj);
+        if (b.x*spriteSize+animAdj[0] < 32) drawSprite(spriteId, [map[0].length-1, b.y], animAdj);
+        if (b.y*spriteSize+animAdj[1] > 32*(map.length-2))    drawSprite(spriteId, [b.x, 0], animAdj);
+        if (b.x*spriteSize+animAdj[0] > 32*(map[0].length-2)) drawSprite(spriteId, [0, b.y], animAdj);
+    })
+    //if (animFrame > 0) ctx.restore();
+}
+
+const finishStep = () => {
+    blizs = advanceBlizzards(blizs);
+
+    // finish moving elf
+    elf.x += moves[elf.action][0];
+    elf.y += moves[elf.action][1];
+
+    elf.hp -= blizs.filter(b => b.x == elf.x && b.y == elf.y).length;
+    stepStartFrame = false;
+    step++;
+}
 
 const draw = () => {
     if (drawing) return;
     drawing = true;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(canvas.offscreenCanvas, 0, 0);
-
-    let anim = 0;
-
+    let animFrame = 0;
     if (stepStartFrame !== false) {
         if (frame - stepStartFrame < 32) {
-            anim = frame - stepStartFrame;
+            animFrame = frame - stepStartFrame;
         } else {
-            // recompute blizs
-            blizs = blizs.map(b => {
-                b.x += moves[b.t][0];
-                b.y += moves[b.t][1];
-
-                if (b.y < 1) b.y = map.length-2;
-                if (b.x < 1) b.x = map[0].length-2;
-                if (b.y > map.length-2) b.y = 1;
-                if (b.x > map[0].length-2) b.x = 1;
-
-                return b;
-            })
-
-            // finish moving elf
-            elf.x += moves[elf.action][0];
-            elf.y += moves[elf.action][1];
-
-            elf.hp -= blizs.filter(b => b.x == elf.x && b.y == elf.y).length;
-            stepStartFrame = false;
-            step++;
+            finishStep();
             if (elf.hp <= 0) restart();
         }
     }
 
-    // draw elf
-    drawSprite(elf.spriteId, elf.x, elf.y, adjust(0, anim, elf.action), adjust(1, anim, elf.action));
-    // scroll to our tricky element
-    let el = document.getElementById('elf');
-    el.style.left = (elf.x*spriteSize-112+adjust(0, anim, elf.action))+'px';
-    el.style.top = (elf.y*spriteSize-64+adjust(1, anim, elf.action))+'px';
-    el.innerHTML = '❤'+elf.hp;//'❤'★
-    if (el.scrollIntoViewIfNeeded) el.scrollIntoViewIfNeeded({behavior: 'smooth', inline: 'center', block: 'center'});
-    else el.scrollIntoView({behavior: 'smooth', inline: 'center', block: 'center'});
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear
+    ctx.drawImage(canvas.planes.ground, 0, 0);
+    drawElf(animFrame);
+    drawBlizzards(animFrame);
+    ctx.drawImage(canvas.planes.walls, 0, 0);
 
-    /*if (anim > 0) {
-        ctx.save();
-        ctx.globalAlpha = 0.5+Math.abs(anim-16)/32;
-    }*/
-
-    blizs.forEach(b => {
-        let spriteId = 3+'<^v>'.indexOf(b.t);
-        drawSprite(spriteId, b.x, b.y, adjust(0, anim, b.t), adjust(1, anim, b.t));
-        
-        // extra winds for the border transitions
-        if (b.y*spriteSize+adjust(1, anim, b.t) < 32)
-            drawSprite(spriteId, b.x, map.length-1, adjust(0, anim, b.t), adjust(1, anim, b.t));
-        if (b.x*spriteSize+adjust(0, anim, b.t) < 32)
-            drawSprite(spriteId, map[0].length-1, b.y, adjust(0, anim, b.t), adjust(1, anim, b.t));
-
-        if (b.y*spriteSize+adjust(1, anim, b.t) > 32*(map.length-2))
-            drawSprite(spriteId, b.x, 0, adjust(0, anim, b.t), adjust(1, anim, b.t));
-        if (b.x*spriteSize+adjust(0, anim, b.t) > 32*(map[0].length-2))
-            drawSprite(spriteId, 0, b.y, adjust(0, anim, b.t), adjust(1, anim, b.t));
-    })
-
-    //if (anim > 0) ctx.restore();
-
-    ctx.drawImage(canvas.offscreenCanvas2, 0, 0);
-
-    frame++;
-    drawing = false;
-
-    // auto run
+    // apply controls
     if (autoRun) {
         if (step < steps.length) action(steps[step]); else autoRun = false;
     } else {
         let keys = Object.entries(keysPressed).filter(([k, v]) => v === true);
         if (keys.length > 0) action(keyMap[keys[0][0]]);
     }
+
+    frame++;
+    drawing = false;
 }
 
 const action = v => {
@@ -225,23 +222,22 @@ const initUI = () => {
 
         restart();
 
-        let p = run(start, end, 0);
-
+        let p = findBestPath(start, end, 0);
         steps = p.steps.slice();
         console.log('part 1', p.t);
-        p = run(end, start, p.t);
+        p = findBestPath(end, start, p.t);
         steps = [...steps, ...p.steps];
-        p = run(start, end, p.t)
+        p = findBestPath(start, end, p.t)
         steps = [...steps, ...p.steps];
         console.log('part 2', p.t);
 
         autoRun = true;
-    })
+    });
     document.getElementById('load').addEventListener('click', e => {
         autoRun = false;
         input = document.getElementById('custom').value;
         restart();
-    })
+    });
 }
 
 sprites.onload = () => {
