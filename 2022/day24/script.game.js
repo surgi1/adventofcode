cf47db0ab9c7f7ef823727724279f4bcad444f36
Jan Surovec <gh1@cloudia.cz>
2023-01-04 09:40:11 +0100

    using composite sprites for character now

--- 2022/day24/script.game.js
+++ 2022/day24/script.game.js
@@ -8,10 +8,10 @@ const moves = {
     'v': [0, 1]
 }
 const elfSpriteIds = {
-    'v': 0,
-    '^': 1,
-    '<': 2,
-    'wait': 0,
+    '^': 0,
+    '<': 1,
+    'v': 2,
+    'wait': 2,
     '>': 3
 }
 const keyMap = {
@@ -34,12 +34,64 @@ let map = [], graves = [], grues = [], dirs = Object.values(moves), movesEntries
     canvas = document.getElementById('canvas'), ctx = canvas.getContext('2d'), spriteSize = 32,
     drawing = false, frame = 0, stepStartFrame = false, keysPressed = {},
     resources = {
-        elfSprites: {url: './elf.png'},
-        sprites: {url: './spritesheet.png'},
-        terrain: {url: './terrain_3.png'}
+        grue: {url: './resources/Grue.png'},
+
+        human: {url: './resources/BODY_male.png'},
+        skeleton: {url: './resources/BODY_skeleton.png'},
+        hood: {url: './resources/HEAD_robe_hood.png'},
+        hood_chain: {url: './resources/HEAD_chain_armor_hood.png'},
+        hair: {url: './resources/HEAD_hair_blonde.png'},
+        hat: {url: './resources/HEAD_leather_armor_hat.png'},
+        helm_chain: {url: './resources/HEAD_chain_armor_helmet.png'},
+        helm_plate: {url: './resources/HEAD_plate_armor_helmet.png'},
+        torso_robe: {url: './resources/TORSO_robe_shirt_brown.png'},
+        torso_plate: {url: './resources/TORSO_plate_armor_torso.png'},
+        torso_leather: {url: './resources/TORSO_leather_armor_torso.png'},
+        torso_chain: {url: './resources/TORSO_chain_armor_torso.png'},
+        torso_purple_jacket: {url: './resources/TORSO_chain_armor_jacket_purple.png'},
+        torso_shirt: {url: './resources/TORSO_leather_armor_shirt_white.png'},
+        shoulders_plate: {url: './resources/TORSO_plate_armor_arms_shoulders.png'},
+        shoulders_leather: {url: './resources/TORSO_leather_armor_shoulders.png'},
+        bracers: {url: './resources/TORSO_leather_armor_bracers.png'},
+        legs_robe: {url: './resources/LEGS_robe_skirt.png'},
+        legs_leather: {url: './resources/LEGS_pants_greenish.png'},
+        legs_plate: {url: './resources/LEGS_plate_armor_pants.png'},
+        gloves_plate: {url: './resources/HANDS_plate_armor_gloves.png'},
+        shoes: {url: './resources/FEET_shoes_brown.png'},
+        shoes_plate: {url: './resources/FEET_plate_armor_shoes.png'},
+        belt: {url: './resources/BELT_leather.png'},
+        belt_rope: {url: './resources/BELT_rope.png'},
+        sprites: {url: './resources/spritesheet.png'},
+        terrain: {url: './resources/terrain_3.png'}
     };
 
 // idea: skeletons (maybe resurrected from the grave?) that would attract the grues in the area
+
+const drawPlayer = (spriteId, [_x, _y]) => {
+    let x = _x -32, y = _y-32;
+    ctx.drawImage(resources.human.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.hair.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+
+    ctx.drawImage(resources.shoes.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.legs_leather.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+
+    ctx.drawImage(resources.torso_chain.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    //ctx.drawImage(resources.torso_purple_jacket.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.shoulders_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.belt.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+
+
+
+    // full metal
+    /*ctx.drawImage(resources.human.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.helm_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.shoes_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.legs_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.torso_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.shoulders_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);
+    ctx.drawImage(resources.gloves_plate.data, spriteId[0]*64, spriteId[1]*64, 64, 64, x, y, 64, 64);*/
+
+}
 
 const createPlane = src => {
     let e = document.createElement('canvas');
@@ -123,16 +175,17 @@ const advanceBlizzards = blizs => blizs.map(b => {
 })
 
 const drawSprite = (spriteId, [x, y]) => ctx.drawImage(resources.sprites.data, spriteId*spriteSize, 0, spriteSize, spriteSize, x, y, spriteSize, spriteSize);
-const drawElfSprite = (spriteId, [x, y]) => ctx.drawImage(resources.elfSprites.data, spriteId[0]*24, spriteId[1]*32, 24, 32, x+4, y, 24, 32);
+//const drawElfSprite = (spriteId, [x, y]) => ctx.drawImage(resources.elfSprites.data, spriteId[0]*24, spriteId[1]*32, 24, 32, x+4, y, 24, 32);
 
 const drawElf = () => {
-    drawElfSprite([elf.action == 'wait' ? 0 : Math.round(frame/4) % 8, elf.spriteId], [elf.x, elf.y]);
+    //drawElfSprite([elf.action == 'wait' ? 0 : Math.round(frame/4) % 8, elf.spriteId], [elf.x, elf.y]);
+    drawPlayer([elf.action == 'wait' ? 0 : 1+Math.round(frame/4) % 8, elf.spriteId], [elf.x, elf.y]);
     
     // scroll page into elf view, update hp
     const params = {behavior: 'smooth', inline: 'center', block: 'center'};
     let el = document.getElementById('elf');
-    el.style.left = (elf.x - 112)+'px';
-    el.style.top = (elf.y - 64)+'px';
+    el.style.left = (elf.x - 128)+'px';
+    el.style.top = (elf.y - 90)+'px';
     el.innerHTML = '❤'+elf.hp;//❤★
     if (el.scrollIntoViewIfNeeded) el.scrollIntoViewIfNeeded(params); else el.scrollIntoView(params);
 }
@@ -144,19 +197,21 @@ const drawBlizzards = () => {
     })
 }
 
+const drawGrue = (spriteId, [x, y]) => ctx.drawImage(resources.grue.data, spriteId*57, 0, 57, 71, x-28, y-35, 57, 71);
+
 const drawGrues = animFrame => {
     ctx.save();
     ctx.globalAlpha = 0.6;
     grues.forEach(g => {
-        let spriteId = 6 + '<v^>'.indexOf(g.t);
-        drawSprite(spriteId, [g.x, g.y]);
+        let spriteId = '>^<v'.indexOf(g.t);
+        drawGrue(spriteId, [g.x, g.y]);
     })
     ctx.restore();
 }
 
 const determineGrueAction = ({x, y, t}, blzs) => {
     let defaultMove = '';
-    if (Math.abs(elf.x-x)+Math.abs(elf.y-y) < 500) {
+    if (Math.abs(elf.x-x)+Math.abs(elf.y-y) < 50) {
         if (Math.abs(elf.x-x) > Math.abs(elf.y-y)) {
             // left or right
             defaultMove = (elf.x > x) ? '>' : '<';
@@ -221,8 +276,8 @@ const draw = () => {
             move[1] += moves[v][1];
         })
         let moveNorm = Math.sqrt(move[0]*move[0]+move[1]*move[1]);
-        move[0] = move[0]/moveNorm;
-        move[1] = move[1]/moveNorm;
+        move[0] = 1.3*move[0]/moveNorm;
+        move[1] = 1.3*move[1]/moveNorm;
 
         elf.spriteId = elfSpriteIds[v];
         let mapY = Math.round((elf.y+move[1])/32),
@@ -250,7 +305,7 @@ const restart = () => {
     start = {x: map[0].indexOf('.'), y: 0};
     end = {x: map[map.length-1].indexOf('.'), y: map.length-1};
     step = 0;
-    elf = {x: start.x*32, y: start.y*32, spriteId: 0, action: 'wait', hp: 100}
+    elf = {x: 32+start.x*32, y: 32+start.y*32, spriteId: 2, action: 'wait', hp: 100}
     blizs = [];
     map.forEach((row, y) => row.forEach((v, x) => {
         if ('<>^v'.indexOf(v) == -1) return true;
