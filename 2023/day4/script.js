e634ccb7d8d6cc6ab27d86555d911af10c3fd781
surgi <surgi1@gmail.com>
2023-12-04 07:46:39 +0100

    d4 polish

--- 2023/day4/script.js
+++ 2023/day4/script.js
@@ -1,16 +1,13 @@
-let p1 = 0, p2 = 0,
-    arr = input.split("\n"),
+let p1 = 0, arr = input.split("\n"),
     copies = Array(arr.length).fill(1);
 
 arr.forEach((line, i) => {
-    let [cardId, body] = line.split(': ');
-    let [win, have] = body.split(' | ').map(s => s.match(/\d+/g).map(Number));
+    let [cardId, body] = line.split(': '),
+        [win, have] = body.split(' | ').map(s => s.match(/\d+/g).map(Number)),
+        score = have.filter(n => win.includes(n)).length;
 
-    let score = have.filter(n => win.includes(n)).length-1;
-
-    if (score > -1) p1 += Math.pow(2, score);
-
-    for (let cp = 0; cp < score+1; cp++) copies[i+1+cp] += copies[i];
+    if (score > 0) p1 += Math.pow(2, score-1);
+    for (let j = 1; j <= score; j++) copies[i+j] += copies[i];
 })
 
 console.log(p1);
