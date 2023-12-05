d771a5a86d3f9377cd0b34236c8497a972ce0bf8
surgi <surgi1@gmail.com>
2023-12-05 23:26:31 +0100

    added non-brute-forced day 5 p2 of 2023

--- 2023/day5/script.js
+++ 2023/day5/script.js
@@ -8,6 +8,12 @@ let rules = arr.map(xform => {
     return tmp.map(l => l.match(/\d+/g).map(Number));
 })
 
+rules.forEach(rule => {
+    rule.sort((a, b) => a[1] - b[1]);
+})
+
+//console.log(rules);
+
 const pipe = v => {
     for (let j = 0; j < rules.length; j++) {
         let rule = rules[j];
@@ -26,9 +32,44 @@ const p1 = () => seeds.map(pipe).sort((a, b) => a-b)[0];
 
 console.log('p1', p1());
 
-// p2 solved in brute force, runs for ~3s in my case; first iterate by jumps of 100 and figure out where most likely the lowest loc is, then run that range by 1.. worked ¯\_(ツ)_/¯
-// TBD revisit and solve in generic, faster and overall better fashion
+// p2 originally solved in brute force, runs for ~3s in my case; first iterate by jumps of 100 and figure out where most likely the lowest loc is, then run that range by 1.. worked ¯\_(ツ)_/¯
+// current implementation jumps the ranges
 const p2 = () => {
+    let min = Number.POSITIVE_INFINITY;
+
+    for (let i = 0; i < seeds.length; i += 2) {
+        let seedToTest = seeds[i];
+
+        while (seedToTest < seeds[i] + seeds[i+1]) {
+
+            min = Math.min(min, pipe(seedToTest));
+
+            let jump = seeds[i] + seeds[i+1] - seedToTest, v = seedToTest;
+            
+            rules.forEach(rule => {
+                let xformed = false;
+
+                for (let j = 0; j < rule.length; j++) {
+                    let r = rule[j];
+                    if (v >= r[1] && v < r[1]+r[2]) {
+                        jump = Math.min(jump, r[1]+r[2]-v);
+                        v = r[0]+v-r[1];
+                        xformed = true;
+                        break;
+                    }
+                }
+
+                if (!xformed) {
+                    if (v < rule[0][1]) jump = rule[0][1] - v;
+                }
+            })
+            seedToTest += jump;
+        }
+    }
+    console.log('p2', min);
+}
+
+const p2Brute = () => {
     let min = Number.POSITIVE_INFINITY, minSeedNr, minPairId;
 
     const processRange = (from, to, i, step, doPairMin = false) => {
