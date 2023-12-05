ca93b6962abaefdfddec4414d21d850c02f8d019
surgi <surgi1@gmail.com>
2023-12-05 08:38:44 +0100

    optimized brute force

--- 2023/day5/script.js
+++ 2023/day5/script.js
@@ -9,12 +9,16 @@ let rules = arr.map(xform => {
 })
 
 const pipe = v => {
-    rules.forEach(rule => rule.some(r => {
-        if (v >= r[1] && v < r[1]+r[2]) {
-            v = r[0]+v-r[1];
-            return true;
+    for (let j = 0; j < rules.length; j++) {
+        let rule = rules[j];
+        for (let i = 0; i < rule.length; i++) {
+            let r = rule[i];
+            if (v >= r[1] && v < r[1]+r[2]) {
+                v = r[0]+v-r[1];
+                break;
+            }
         }
-    }))
+    }
     return v;
 }
 
@@ -22,30 +26,39 @@ const p1 = () => seeds.map(pipe).sort((a, b) => a-b)[0];
 
 console.log('p1', p1());
 
-// p2 solved in very dummy approach; first iterate by jumps of 10 and figure out where most likely the lowest loc is, then run that range by 1.. worked ¯\_(ツ)_/¯
+// p2 solved in brute force, runs for ~3s in my case; first iterate by jumps of 100 and figure out where most likely the lowest loc is, then run that range by 1.. worked ¯\_(ツ)_/¯
 // TBD revisit and solve in generic, faster and overall better fashion
 const p2 = () => {
-    let min = 100000000000, minSeedNr, minPairId;
+    let min = Number.POSITIVE_INFINITY, minSeedNr, minPairId;
 
-    const processRange = (i, step) => {
+    const processRange = (from, to, i, step, doPairMin = false) => {
         console.log('processing pair', i);
-        let seed = seeds[i*2];
-        while (seed < seeds[i*2] + seeds[i*2+1]) {
+        let pairMin = Number.POSITIVE_INFINITY;
+        let seed = from;
+        if (seed < seeds[i*2]) seed = seeds[i*2];
+        if (to > seeds[i*2] + seeds[i*2+1]) to = seeds[i*2] + seeds[i*2+1];
+        while (seed < to) {
             let m = pipe(seed);
             if (m < min) {
                 min = m;
                 minSeedNr = seed;
                 minPairId = i;
-                console.log('min so far', min, minSeedNr);
+                //console.log('min so far', min, minSeedNr);
             }
+            if (m < pairMin) pairMin = m;
             seed += step;
         }
+        console.log('global min so far', min);
+        if (doPairMin) console.log('pair minimum distance from global minimum', pairMin - min);
     }
 
-    for (let i = 0; i < seeds.length/2; i++) processRange(i, 10);
+    for (let i = 0; i < seeds.length/2; i++) processRange(seeds[i*2], seeds[i*2] + seeds[i*2+1], i, 100, true);
 
     console.log('part 2, step 2');
-    processRange(minPairId, 1);
+    processRange(minSeedNr-100, minSeedNr+100, minPairId, 1);
+    console.log('p2', min);
 }
 
+console.time('cmp');
 p2();
+console.timeEnd('cmp')
