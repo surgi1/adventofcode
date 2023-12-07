b4e3f91c85eb4fcb5c4be8b973c4e8da928346fc
surgi <surgi1@gmail.com>
2023-12-07 06:55:37 +0100

    d7 raw

--- 2023/day6/script.js
+++ 2023/day6/script.js
@@ -1,17 +1,17 @@
 const p1 = () => {
-	let arr = input.split("\n").map(line => line.match(/\d+/g).map(Number));
-	return arr[0].reduce((a, v, i) => {
-		let ways = 0;
-		for (let j = 1; j < v-1; j++) if (j * (v-j) > arr[1][i]) ways++;
-		return a*ways;
-	}, 1);
+    let arr = input.split("\n").map(line => line.match(/\d+/g).map(Number));
+    return arr[0].reduce((a, v, i) => {
+        let ways = 0;
+        for (let j = 1; j < v-1; j++) if (j * (v-j) > arr[1][i]) ways++;
+        return a*ways;
+    }, 1);
 }
 
 const p2 = () => {
-	let arr = input.split("\n").map(line => line.match(/\d+/g).join('')).map(Number);
-	let ways = 0;
-	for (let j = 1; j < arr[0]-1; j++) if (j * (arr[0]-j) > arr[1]) ways++;
-	return ways;
+    let arr = input.split("\n").map(line => line.match(/\d+/g).join('')).map(Number);
+    let ways = 0;
+    for (let j = 1; j < arr[0]-1; j++) if (j * (arr[0]-j) > arr[1]) ways++;
+    return ways;
 }
 
 console.log('p1', p1());
---
+++ 2023/day7/script.part1.js
@@ -0,0 +1,42 @@
+// p1
+const cardStr = {
+    A: 14, K: 13, Q: 12, J:11, T: 10, 9: 9, 8: 8, 7: 7, 6: 6, 5: 5, 4: 4, 3: 3, 2: 2
+}
+
+let hands = [];
+input.split("\n").map(line => {
+    let tmp = line.split(' ');
+    let vals = tmp[0].split('');
+    let groups = [];
+    
+    vals.forEach(v => {
+        let g = groups.filter(g => g.val == v)?.[0];
+        if (!g) groups.push({val: v, cnt: 1}); else g.cnt++;
+    })
+
+    groups.sort((a, b) => b.cnt - a.cnt);
+
+    let str;
+    switch (groups.length) {
+        case 1: str = 7; break;
+        case 2: str = (groups[0].cnt == 4 ? 6 : 5); break;
+        case 3: str = (groups[0].cnt == 3 ? 4 : 3); break;
+        case 4: str = 2; break;
+        case 5: str = 1; break;
+    }
+    
+    hands.push({
+        raw: tmp[0],
+        bid: Number(tmp[1]),
+        groups: groups,
+        str: str,
+    })
+})
+
+hands.sort((a, b) => {
+    if (a.str != b.str) return a.str-b.str;
+    for (let i = 0; i < 5; i++)
+        if (a.raw[i] != b.raw[i]) return cardStr[a.raw[i]] - cardStr[b.raw[i]];
+})
+
+console.log( hands.reduce((a, v, i) => a + (i+1)*v.bid, 0) )
