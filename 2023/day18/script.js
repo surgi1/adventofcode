3a1e2155830079d13b94164f832b1467af67d075
surgi <surgi1@gmail.com>
2023-12-18 11:57:41 +0100

    d18 minor polish

--- 2023/day18/script.js
+++ 2023/day18/script.js
@@ -1,17 +1,7 @@
 // idea for part 2: adaptive grid in x and y, works for part 1 as well
-const D = {
-    R: 0,
-    D: 1,
-    L: 2, 
-    U: 3,
-}
 
-const DS = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-]
+const DS = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+const  D = { R: 0,   D: 1,   L: 2,    U: 3 };
 
 const addVect = (a, b) => a.map((v, c) => v+b[c]);
 const mulVect = (a, o) => o.map(v => a*v);
@@ -23,9 +13,6 @@ const parseInputPart1 = (pos = [0, 0]) => input.split("\n").map(line => {
 
     return {
         dir: D[dir],
-        len: Number(len),
-        posFrom: posFrom,
-        posTo: pos,
         min: [0, 1].map(c => Math.min(posFrom[c], pos[c])),
         max: [0, 1].map(c => Math.max(posFrom[c], pos[c])),
     }
@@ -33,17 +20,13 @@ const parseInputPart1 = (pos = [0, 0]) => input.split("\n").map(line => {
 
 const parseInputPart2 = (pos = [0, 0]) => input.split("\n").map(line => {
     let [dir, len, rgb] = line.split(' ');
-    rgb = rgb.slice(1, -1);
-    dir = Number(rgb.slice(-1));
-    len = parseInt(rgb.slice(1, -1), 16)
+    dir = Number(rgb.slice(-2, -1));
+    len = parseInt(rgb.slice(2, -2), 16);
     let posFrom = [...pos];
     pos = addVect(mulVect(len, DS[dir]), pos);
 
     return {
         dir: dir,
-        len: len,
-        posFrom: posFrom,
-        posTo: pos,
         min: [0, 1].map(c => Math.min(posFrom[c], pos[c])),
         max: [0, 1].map(c => Math.max(posFrom[c], pos[c])),
     }
@@ -54,15 +37,12 @@ const run = (digs, canvas) => {
         squares = [], grid = [[], []],
         volume = 0, trench = 0;
 
-    for (let i = 0; i < digs.length; i++) {
-        let dig = digs[i];
-        [0, 1].forEach(c => {
-            if (!grid[c].includes(dig.posFrom[c])) grid[c].push(dig.posFrom[c]);
-        })
+    digs.forEach(dig => {
+        dig.min.forEach((v, c) => (!grid[c].includes(v)) && grid[c].push(v));
         max = max.map((v, c) => Math.max(v, dig.max[c]));
         min = min.map((v, c) => Math.min(v, dig.min[c]));
-        trench += dig.len;
-    }
+        trench += dig.max.reduce((a, v, c) => a+(v-dig.min[c]), 0);
+    })
 
     grid.forEach(c => c.sort((a, b) => a-b));
 
@@ -70,11 +50,12 @@ const run = (digs, canvas) => {
         // calc middle of the square
         let pos = [(grid[0][i+1] + grid[0][i])/2, (grid[1][j+1] + grid[1][j])/2];
 
-        // now we need to determine whether the center is inside
-        if ([digs.filter(o => [D.L, D.R].includes(o.dir) && o.posTo[1] < pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0]).length,
-             digs.filter(o => [D.L, D.R].includes(o.dir) && o.posTo[1] > pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0]).length,
-             digs.filter(o => [D.U, D.D].includes(o.dir) && o.posTo[0] < pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1]).length,
-             digs.filter(o => [D.U, D.D].includes(o.dir) && o.posTo[0] > pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1]).length].some(o => o % 2 == 0)) continue;
+        // now we need to determine whether the center is inside - we do that by counting crosses to each border. Is this the shoelace theorem?
+        // min[0] == max[0] for U/D, min[1] == max[1] for L/R
+        if ([digs.filter(o => [D.L, D.R].includes(o.dir) && o.min[1] < pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0]).length,
+             digs.filter(o => [D.L, D.R].includes(o.dir) && o.min[1] > pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0]).length,
+             digs.filter(o => [D.U, D.D].includes(o.dir) && o.min[0] < pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1]).length,
+             digs.filter(o => [D.U, D.D].includes(o.dir) && o.min[0] > pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1]).length].some(o => o % 2 == 0)) continue;
 
         volume += Math.abs(grid[0][i+1] - grid[0][i])*Math.abs(grid[1][j+1] - grid[1][j]);
         squares.push([grid[0][i], grid[0][i+1], grid[1][j], grid[1][j+1]]);
@@ -112,11 +93,13 @@ const draw = (canvas, min, max, grid, digs, squares) => {
 
     // trenches
     ctx.strokeStyle = '#f00';
-    ctx.beginPath();
-    ctx.moveTo(digs[0].posFrom[0]*zoom, digs[0].posFrom[1]*zoom);
-    digs.forEach(o => ctx.lineTo(o.posTo[0]*zoom, o.posTo[1]*zoom))
     ctx.lineWidth = 1.0;
-    ctx.stroke();
+    digs.forEach(o => {
+        ctx.beginPath();
+        ctx.moveTo(o.min[0]*zoom, o.min[1]*zoom);
+        ctx.lineTo(o.max[0]*zoom, o.max[1]*zoom)
+        ctx.stroke();
+    })
 
     ctx.globalAlpha = 0.2;
     // fill
