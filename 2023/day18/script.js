fe422500e2ea7fdf058c94a16e9aca5f1cbeb7e0
surgi <surgi1@gmail.com>
2023-12-18 11:25:20 +0100

    d18

--- 2023/day18/script.js
+++ 2023/day18/script.js
@@ -18,15 +18,13 @@ const mulVect = (a, o) => o.map(v => a*v);
 
 const parseInputPart1 = (pos = [0, 0]) => input.split("\n").map(line => {
     let [dir, len, rgb] = line.split(' ');
-
-    len = Number(len);
     let posFrom = [...pos];
-    pos = addVect(mulVect(len, DS[D[dir]]), pos);
+    pos = addVect(mulVect(Number(len), DS[D[dir]]), pos);
 
     return {
         dir: D[dir],
+        len: Number(len),
         posFrom: posFrom,
-        len: len,
         posTo: pos,
         min: [0, 1].map(c => Math.min(posFrom[c], pos[c])),
         max: [0, 1].map(c => Math.max(posFrom[c], pos[c])),
@@ -43,8 +41,8 @@ const parseInputPart2 = (pos = [0, 0]) => input.split("\n").map(line => {
 
     return {
         dir: dir,
-        posFrom: posFrom,
         len: len,
+        posFrom: posFrom,
         posTo: pos,
         min: [0, 1].map(c => Math.min(posFrom[c], pos[c])),
         max: [0, 1].map(c => Math.max(posFrom[c], pos[c])),
@@ -52,15 +50,14 @@ const parseInputPart2 = (pos = [0, 0]) => input.split("\n").map(line => {
 })
 
 const run = (digs, canvas) => {
-    let max = [0, 0], min = [0, 0];
-    let squares = [], grid = [[], []];
-    let volume = 0, trench = 0;
+    let max = [0, 0], min = [0, 0],
+        squares = [], grid = [[], []],
+        volume = 0, trench = 0;
 
     for (let i = 0; i < digs.length; i++) {
         let dig = digs[i];
         [0, 1].forEach(c => {
             if (!grid[c].includes(dig.posFrom[c])) grid[c].push(dig.posFrom[c]);
-            if (!grid[c].includes(dig.posTo[c])) grid[c].push(dig.posTo[c]);
         })
         max = max.map((v, c) => Math.max(v, dig.max[c]));
         min = min.map((v, c) => Math.min(v, dig.min[c]));
@@ -71,20 +68,16 @@ const run = (digs, canvas) => {
 
     for (let i = 0; i < grid[0].length-1; i++) for (let j = 0; j < grid[1].length-1; j++) {
         // calc middle of the square
-        let pos = [];
-        pos[0] = (grid[0][i+1] + grid[0][i])/2;
-        pos[1] = (grid[1][j+1] + grid[1][j])/2;
-        // now we need to determine whether the center is inside
-        let crosses = [
-            digs.filter(o => [D.L, D.R].includes(o.dir) && o.posFrom[1] < pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0] ).length,
-            digs.filter(o => [D.L, D.R].includes(o.dir) && o.posFrom[1] > pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0] ).length,
-            digs.filter(o => [D.U, D.D].includes(o.dir) && o.posFrom[0] < pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1] ).length,
-            digs.filter(o => [D.U, D.D].includes(o.dir) && o.posFrom[0] > pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1] ).length
-        ];
+        let pos = [(grid[0][i+1] + grid[0][i])/2, (grid[1][j+1] + grid[1][j])/2];
 
-        if (crosses.some(o => o % 2 == 0)) continue;
+        // now we need to determine whether the center is inside
+        if ([digs.filter(o => [D.L, D.R].includes(o.dir) && o.posTo[1] < pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0]).length,
+             digs.filter(o => [D.L, D.R].includes(o.dir) && o.posTo[1] > pos[1] && o.min[0] < pos[0] && o.max[0] > pos[0]).length,
+             digs.filter(o => [D.U, D.D].includes(o.dir) && o.posTo[0] < pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1]).length,
+             digs.filter(o => [D.U, D.D].includes(o.dir) && o.posTo[0] > pos[0] && o.min[1] < pos[1] && o.max[1] > pos[1]).length].some(o => o % 2 == 0)) continue;
+
         volume += Math.abs(grid[0][i+1] - grid[0][i])*Math.abs(grid[1][j+1] - grid[1][j]);
-        squares.push({bounds: [grid[0][i], grid[0][i+1], grid[1][j], grid[1][j+1]], mode: 'in'});
+        squares.push([grid[0][i], grid[0][i+1], grid[1][j], grid[1][j+1]]);
     }
 
     if (canvas !== undefined) draw(canvas, min, max, grid, digs, squares);
@@ -100,36 +93,34 @@ const draw = (canvas, min, max, grid, digs, squares) => {
     canvas.height = (max[1] - min[1] + 1)*zoom;
 
     ctx.translate(-min[0]*zoom, -min[1]*zoom);
-    ctx.strokeStyle = '#ff0000';
+    
+    // grid
+    ctx.strokeStyle = '#888';
+    ctx.lineWidth = 0.2;
     grid[0].forEach(x => {
         ctx.beginPath();
         ctx.moveTo(x*zoom, min[1]*zoom);
         ctx.lineTo(x*zoom, max[1]*zoom);
-        ctx.lineWidth = 0.2;
         ctx.stroke();
     })
     grid[1].forEach(y => {
         ctx.beginPath();
         ctx.moveTo(min[0]*zoom, y*zoom);
         ctx.lineTo(max[0]*zoom, y*zoom);
-        ctx.lineWidth = 0.2;
         ctx.stroke();
     })
 
-    ctx.strokeStyle = '#800000';
+    // trenches
+    ctx.strokeStyle = '#f00';
+    ctx.beginPath();
+    ctx.moveTo(digs[0].posFrom[0]*zoom, digs[0].posFrom[1]*zoom);
+    digs.forEach(o => ctx.lineTo(o.posTo[0]*zoom, o.posTo[1]*zoom))
+    ctx.lineWidth = 1.0;
+    ctx.stroke();
+
     ctx.globalAlpha = 0.2;
-    digs.forEach(o => {
-        ctx.beginPath();
-        ctx.moveTo(o.posFrom[0]*zoom, o.posFrom[1]*zoom);
-        ctx.lineTo(o.posTo[0]*zoom, o.posTo[1]*zoom);
-        ctx.lineWidth = 2.0;
-        ctx.stroke();
-    })
-
-    squares.forEach(o => {
-        ctx.fillStyle = o.mode == 'in' ? '#00f' : '#f00';
-        ctx.fillRect(o.bounds[0]*zoom, o.bounds[2]*zoom, (o.bounds[1] - o.bounds[0])*zoom, (o.bounds[3] - o.bounds[2])*zoom);
-    })
+    // fill
+    squares.forEach(bounds => ctx.fillRect(bounds[0]*zoom, bounds[2]*zoom, (bounds[1] - bounds[0])*zoom, (bounds[3] - bounds[2])*zoom))
 }
 
 console.log('p1', run(parseInputPart1(), document.getElementById('part1')));
