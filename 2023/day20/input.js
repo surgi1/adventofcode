0cd16c865460f5d2da240f2ca07c94bb2a654f5c
surgi <surgi1@gmail.com>
2023-12-20 10:09:12 +0100

    d20 semi manual

---
+++ 2023/day20/input.js
@@ -0,0 +1,70 @@
+let inputt = `broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a`;
+
+let input2 = `broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output`;
+
+let input = `&ls -> fg, ck, fv, sk, fl, hz
+%px -> ls, dv
+%jk -> xt, tx
+%hd -> fs, vn
+%pk -> ql
+%bj -> tx, pr
+%vg -> xl, sf
+%cj -> bj
+%sk -> bz
+%fl -> fx
+%th -> fl, ls
+%pr -> gm
+%xv -> sf, hp
+%mh -> jb
+%jh -> kx, tx
+%jz -> pm, fs
+%hr -> tx, gk
+%kx -> cj
+%ql -> jx
+%gm -> tx, jt
+%hz -> ls, fv
+%dt -> fs
+%gg -> sf, vg
+%xl -> fh
+&pq -> vr
+%jx -> mv
+%kr -> gg
+%bn -> px, ls
+&fs -> pm, rg, pq, pj, nk, mh, jb
+%vn -> rt, fs
+%jt -> tx, zb
+broadcaster -> hz, hr, nk, nv
+%fx -> sk, ls
+%rt -> fs, kz
+%gk -> tx, jh
+%mv -> sf, kr
+%bz -> bn, ls
+%dv -> ls
+%rg -> jz
+%pj -> mh
+%kz -> dt, fs
+%ck -> hg
+&fg -> vr
+&sf -> pk, jx, nv, kr, xl, ql, dk
+%jb -> rg
+%nk -> fs, pj
+&dk -> vr
+%hp -> sf
+&tx -> hr, fm, kx, cj, pr
+%nv -> pk, sf
+%fh -> xv, sf
+%xt -> tx
+%hg -> ls, th
+%zb -> tx, jk
+&fm -> vr
+%pm -> hd
+%fv -> ck
+&vr -> rx`;
