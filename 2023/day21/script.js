a0a486f8af02ed58da8fd4f84b0001d45c91179d
surgi <surgi1@gmail.com>
2023-12-21 12:32:00 +0100

    day21

--- 2023/day21/script.js
+++ 2023/day21/script.js
@@ -5,6 +5,7 @@
 // we need to get to first 3 i's, then figure out the polynomial
 // reached(steps) in my case as a fnc of i: i=0: 3703, i=1: 32957, i=2: 91379
 // originally plugged this into https://www.dcode.fr/newton-interpolating-polynomial :shrug:
+// wolfram alpha is also a good friend here: https://www.wolframalpha.com/input?i=quadratic+fit+calculator&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data3x%22%7D+-%3E%22%7B0%2C+1%2C+2%7D%22&assumption=%7B%22F%22%2C+%22QuadraticFitCalculator%22%2C+%22data3y%22%7D+-%3E%22%7B3703%2C+32957%2C+91379%7D%22
 // later figured out we can use day9 and made the solution return actual result
 
 const mod = (n, m) => ((n % m) + m) % m;
@@ -56,7 +57,7 @@ for (let i = 1; i <= 131*2+65; i++) {
 
     if (i % 131 == 65) {
         vals.push(Object.keys(pos).length);
-        console.log(i, Object.keys(pos).length);
+        console.log(i, (i-65)/131, Object.keys(pos).length);
     }
 }
 
