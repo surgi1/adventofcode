16d8a06c48247fa05117365f8b185e67c5ee7b68
surgi <surgi1@gmail.com>
2024-07-13 23:14:29 +0200

    redone some random unsaved puzzles

--- 2017/day4/script.js
+++ 2017/day4/script.js
@@ -1,7 +1,7 @@
 const arrayCmp = (a, b) => a.length === b.length && a.every(v => b.includes(v))
 const validate = (cmpFnc) => input.split('\n').filter(line => {
     let tmp = line.split(' ');
-    return !tmp.some((o, i) => tmp.some((b, j) => cmpFnc(o, b) && i !== j))
+    return !tmp.some((a, i) => tmp.filter((b, j) => i !== j).some(b => cmpFnc(a, b)))
 }).length
 
 console.log('p1', validate((a, b) => a === b));
