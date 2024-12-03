98d24e6e05236f600c85cde8e288e78a9974657a
surgi <surgi1@gmail.com>
2024-12-03 09:12:19 +0100

    d3

--- 2024/day3/script.js
+++ 2024/day3/script.js
@@ -1,9 +1,9 @@
 const run = s => s.match(/mul\(\d+,\d+\)/g).reduce((res, cmd) => res + cmd.match(/\d+/g).reduce((a, v) => a*v, 1), 0)
 
-const p2 = (input, mult = 1) => input.match(/don't\(\)|do\(\)|mul\(\d+,\d+\)/g).reduce((res, cmd) => {
-    if (cmd == "don't()") {mult = 0; return res}
-    if (cmd == "do()") {mult = 1; return res}
-    return res + (mult ? run(cmd) : 0)
+const p2 = (input, gate = 1) => input.match(/don't\(\)|do\(\)|mul\(\d+,\d+\)/g).reduce((res, cmd) => {
+    if (cmd == "don't()") {gate = 0; return res}
+    if (cmd == "do()") {gate = 1; return res}
+    return res + (gate ? run(cmd) : 0)
 }, 0)
 
 console.log('p1', run(input));
