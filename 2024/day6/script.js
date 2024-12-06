008fe584591e01d5b15f4ebd651b06c7fe7f5f9f
surgi <surgi1@gmail.com>
2024-12-06 19:29:16 +0100

    d6

--- 2024/day6/script.js
+++ 2024/day6/script.js
@@ -1,7 +1,6 @@
 let DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
-const init = input => {
-    let guard;
+const init = (input, guard) => {
     let map = input.split("\n").map((line, y) => line.split('').map((v, x) => {
         if (v == '^') guard = {x: x, y: y, dir: 0};
         return v == '#' ? 1 : 0;
@@ -9,43 +8,38 @@ const init = input => {
     return [guard, map]
 }
 
-const run = ([guard, map], ox = false, oy = false) => {
+const run = (guard, map, ox = false, oy = false) => {
     const onMap = (x, y) => x >= 0 && y >= 0 && x < cols && y < rows;
+    const k = o => o.y+'_'+o.x+'_'+o.dir;
 
     let rows = map.length, cols = map[0].length,
-        spots = {}, states = {}, loop = false;
+        spots = {}, states = {}, x, y;
 
     if (ox !== false) map[oy][ox] = 1;
     
-    while (!loop) {
+    while (states[k(guard)] == undefined && onMap(guard.x, guard.y)) {
         spots[guard.y+'_'+guard.x] = [guard.x, guard.y];
-        states[guard.y+'_'+guard.x+'_'+guard.dir] = 1;
+        states[k(guard)] = 1;
 
-        let x = guard.x+DIRS[guard.dir][0],
-            y = guard.y+DIRS[guard.dir][1];
+        x = guard.x+DIRS[guard.dir][0];
+        y = guard.y+DIRS[guard.dir][1];
 
         while (onMap(x, y) && map[y][x] == 1) {
-            guard.dir = (guard.dir+1) % 4;
+            guard.dir = ++guard.dir % 4;
             x = guard.x+DIRS[guard.dir][0];
             y = guard.y+DIRS[guard.dir][1];
         }
 
-        if (states[y+'_'+x+'_'+guard.dir] !== undefined) loop = true;
-
-        if (onMap(x, y)) {
-            if (map[y][x] == 0) {
-                guard.x = x;
-                guard.y = y;
-            }
-        } else break;
+        guard.x = x;
+        guard.y = y;
     }
 
     if (ox !== false) map[oy][ox] = 0;
 
-    return ox !== false ? loop : Object.values(spots);
+    return ox !== false ? states[k(guard)] !== undefined : Object.values(spots);
 }
 
-const p2 = ([guard, map]) => run([{...guard}, map]).filter(([x, y]) => (x != guard.x || y != guard.y) && run([{...guard}, map], x, y))
+const p2 = (guard, map) => run({...guard}, map).filter(([x, y]) => (x != guard.x || y != guard.y) && run({...guard}, map, x, y))
 
-console.log('p1', run(init(input)).length);
-console.log('p2', p2(init(input)).length);
+console.log('p1', run(...init(input)).length);
+console.log('p2', p2(...init(input)).length);
