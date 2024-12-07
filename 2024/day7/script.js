ed733c6f95c3772b28743b7ed3e51ecc9aba063b
surgi <surgi1@gmail.com>
2024-12-07 09:01:05 +0100

    d7

--- 2024/day7/script.js
+++ 2024/day7/script.js
@@ -2,26 +2,21 @@ const init = input => input.split("\n").map((line, y) => line.match(/\d+/g).map(
 
 const processLine1 = (cmpVal, _arr, tmpRes) => {
     let arr = [..._arr];
-    if (tmpRes === undefined) tmpRes = arr.pop();
-    if (arr.length == 0) return cmpVal == tmpRes;
-    let n = arr.pop();
+    if (tmpRes === undefined) tmpRes = arr.shift();
+    if (arr.length == 0) return cmpVal == tmpRes ? cmpVal : 0;
+    let n = arr.shift();
     return processLine1(cmpVal, arr, tmpRes*n) || processLine1(cmpVal, arr, tmpRes+n)
 }
 
 const processLine = (cmpVal, _arr, tmpRes) => {
     let arr = [..._arr];
-    if (tmpRes === undefined) tmpRes = arr.pop();
-    if (arr.length == 0) return cmpVal == tmpRes;
-    let n = arr.pop();
+    if (tmpRes === undefined) tmpRes = arr.shift();
+    if (arr.length == 0) return cmpVal == tmpRes ? cmpVal : 0;
+    let n = arr.shift();
     return processLine(cmpVal, arr, tmpRes*n) || processLine(cmpVal, arr, tmpRes+n) || processLine(cmpVal, arr, Number(tmpRes+''+n))
 }
 
-const run = (data, cmpFnc) => data.reduce((res, line) => {
-    line = line.reverse();
-    let cmpVal = line.pop();
-    return res + (cmpFnc(cmpVal, line) ? cmpVal : 0);
-}, 0)
-
+const run = (data, cmpFnc) => data.reduce((a, line) => a + cmpFnc(line.shift(), line), 0)
 
 console.log('p1', run(init(input), processLine1));
 console.log('p2', run(init(input), processLine));
