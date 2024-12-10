3d80fa94ebd72417ed090df40e49515a9f0e94df
surgi <surgi1@gmail.com>
2024-12-10 08:17:45 +0100

    d10

---
+++ 2024/day10/README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2024 - Day 10
+
+https://adventofcode.com/2024/day/10
+
+JavaScript implementation
+
+Check it live at https://surgi1.github.io/adventofcode/2024/day10/index.html
---
+++ 2024/day10/script.js
@@ -0,0 +1,40 @@
+const init = input => input.split('\n').map(line => line.split('').map(Number))
+
+const DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+
+const headScores = (map, sx, sy, p2 = false) => {
+    let res = {}, count = 0;
+    let stack = [[sx, sy]], cur, cols = map[0].length, rows = map.length;
+
+    const offMap = (x, y) => x < 0 || y < 0 || x >= cols || y >= rows;
+
+    while (cur = stack.pop()) {
+        let [x, y] = cur;
+        let v = map[y][x];
+        if (v == 9) {
+            res[cur.join('_')] = 1;
+            count++;
+            continue;
+        }
+        DIRS.forEach(([dx, dy]) => {
+            let nx = x+dx, ny = y+dy;
+            if (offMap(nx, ny)) return true;
+            if (map[ny][nx] != v+1) return true;
+            stack.push([nx, ny]);
+        })
+    }
+
+    return p2 ? count : Object.values(res).length
+}
+
+const run = (map, p2 = false, res = 0) => {
+    map.forEach((row, y) => row.forEach((v, x) => {
+        if (v !== 0) return true;
+        res += headScores(map, x, y, p2);
+    }))
+    return res;
+}
+
+
+console.log('p1', run(init(input)));
+console.log('p2', run(init(input), true));
