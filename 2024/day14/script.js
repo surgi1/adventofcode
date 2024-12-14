6abf31489a5ed6b1851c904afa887cfa69347573
surgi <surgi1@gmail.com>
2024-12-14 20:45:40 +0100

    minor cleanup

--- 2024/day14/script.js
+++ 2024/day14/script.js
@@ -18,9 +18,9 @@ const run = (data, cols, rows, t = 100) => {
     let colsHalf = Math.floor(cols/2);
     let rowsHalf = Math.floor(rows/2);
     
-    data.forEach(([x, y, vx, vy]) => {
-        x = modulo(x + vx*t, cols);
-        y = modulo(y + vy*t, rows);
+    data.forEach(([sx, sy, vx, vy]) => {
+        let x = modulo(sx + vx*t, cols);
+        let y = modulo(sy + vy*t, rows);
         if (x == colsHalf || y == rowsHalf) return true;
         quadrants[y < rowsHalf ? 0 : 1][x < colsHalf ? 0 : 1]++;
     })
@@ -32,7 +32,7 @@ const run2 = (data, cols, rows) => {
     resizeCanvas();
 
     // what are each bots periodicities?
-    // for some reason it is rows*cols = 10403, I had to compute it though
+    // for some reason it is rows*cols = 10403, I had to run the simulation to check
 
     let t = 0;
     let periodHoriz = rows; // rows; had to determine by eye
@@ -42,13 +42,14 @@ const run2 = (data, cols, rows) => {
     // candidate for correct t Horiz: 65, 65+rows, 65+2*rows, 65+3*rows, ...
     // candidate for correct t Vert: 11, 11+cols, 11+2*cols, 11+3*cols, ...
     // the correct t must satisfy t % cols = 11 and t % rows = 65
-    // surely there is a better way to compute think, modulo math basic knowledge (lack of) strikes again
-    for (let n = 0; n < 10403; n++) {
-        if (n % periodVert == startVert && n % periodHoriz == startHoriz) {
-            t = n;
-            break;
-        }
-    }
+    // surely there is a better way to compute this, modulo math basic knowledge (lack of thereof) strikes again
+    /*for (let n = startVert; n < 10403; n += periodVert) {
+        if (n % periodHoriz == startHoriz) { t = n; break;}
+    }*/
+
+    // theres actually this one that works: see that it omits periodHoriz completely... why??
+    t = startVert + periodVert*(startVert+startHoriz);
+
     const loop = () => {
         ctx.fillStyle = '#fff';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -67,7 +68,8 @@ const run2 = (data, cols, rows) => {
     }
 
     requestAnimationFrame(loop);
+    return t;
 }
 
 console.log('p1', run(init(input), 101, 103));
-run2(init(input), 101, 103);
+console.log('p2', run2(init(input), 101, 103));
