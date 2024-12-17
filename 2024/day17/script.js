3ff0b7a0b8c7419e3159186ec9ee9b92197835d3
surgi <surgi1@gmail.com>
2024-12-17 17:47:08 +0100

    reworked into automated recursive solution

--- 2024/day17/script.js
+++ 2024/day17/script.js
@@ -8,17 +8,10 @@ Object.defineProperty(Array.prototype, 'chunk', {
 
 const init = input => input.match(/\d+/g).map(Number);
 
-const part1 = (data, rega) => {
-    let regs = [0,0,0];
-    regs = regs.map(i => data.shift());
-    if (rega !== undefined) regs[0] = rega;
-    let ptr = 0;
-    let program = data.chunk(2);
-    let programLit = data.join(',');
-
-    let out = [];
-
-    const output = v => out.push(v);
+const part1 = (a, b, c, ...programFull) => {
+    let ptr = 0, out = [];
+    let regs = [a, b, c];
+    let program = programFull.chunk(2);
 
     while (ptr < program.length) {
         let [code, op] = program[ptr];
@@ -31,7 +24,7 @@ const part1 = (data, rega) => {
             case 2: regs[1] = combo % 8; ptr++; break;
             case 3: if (regs[0] != 0) ptr = literal; else ptr++; break;
             case 4: regs[1] = (regs[1] ^ regs[2]) >>> 0; ptr++; break;
-            case 5: output(combo % 8); ptr++; break;
+            case 5: out.push(combo % 8); ptr++; break;
             case 6: regs[1] = Math.trunc(regs[0]/(2**combo)); ptr++; break;
             case 7: regs[2] = Math.trunc(regs[0]/(2**combo)); ptr++; break;
         }
@@ -47,7 +40,7 @@ const toDec = s => parseInt(s, 8);
 2,4 ; regs[1] = regs[0] % 8;
 1,1 ; regs[1] = regs[1] ^ 1;
 7,5 ; regs[2] = Math.trunc(regs[0]/(2**regs[1]));
-0,3 ; regs[0] = Math.trunc(regs[0]/8); // regs[0] = regs[0] >> 3;
+0,3 ; regs[0] = Math.trunc(regs[0]/8);
 1,4 ; regs[1] = regs[1] ^ 4;
 4,4 ; regs[1] = regs[1] ^ regs[2];
 5,5 ; output(regs[1] % 8);
@@ -67,12 +60,11 @@ while (regs[0] != 0) {
 
 // also, JS is a bitch and fucks up XORs with 64-bit integers; see >>> 0 in the code
 
-const part2 = data => {
-    let programFull = [2,4,1,1,7,5,0,3,1,4,4,4,5,5,3,0];
+const part2 = (a, b, c, ...programFull) => {
     let program = programFull.chunk(2);
 
     const test = a => {
-        let ptr = 0, regs = [a, 0, 0], d = 0;
+        let ptr = 0, regs = [a, 0, 0], matches = 0;
 
         while (ptr < program.length) {
             let [code, op] = program[ptr];
@@ -86,8 +78,8 @@ const part2 = data => {
                 case 3: if (regs[0] != 0) ptr = literal; else ptr++; break;
                 case 4: regs[1] = (regs[1] ^ regs[2]) >>> 0; ptr++; break;
                 case 5: 
-                    if (programFull[d] != (combo % 8)) return d;
-                    d++;
+                    if (programFull[matches] != (combo % 8)) return matches;
+                    matches++;
                     ptr++;
                     break;
                 case 6: regs[1] = Math.trunc(regs[0]/(2**combo)); ptr++; break;
@@ -95,7 +87,7 @@ const part2 = data => {
             }
         }
 
-        return d;
+        return matches;
     }
 
     let min = Infinity;
@@ -107,15 +99,14 @@ const part2 = data => {
         }
         for (let a = 0; a < 8; a++) {
             let oct = a+base;
-            let newMatches = test( toDec(oct) );
+            let newMatches = test(toDec(oct));
             if (newMatches > matches || (oct.length - matches) < 3) recur(oct, newMatches); // magic nr 3 comes from the way digits are tangled in the output generation; see the intcode above
         }
     }
 
     recur('');
-
     return min;
 }
 
-console.log('p1', part1(init(input)))
-console.log('p2', part2(init(input)))
+console.log('p1', part1(...init(input)))
+console.log('p2', part2(...init(input)))
