69e6ce4d31d2b4a8e5be3d2209473f16b2a677f9
surgi <surgi1@gmail.com>
2025-12-12 07:49:07 +0100

    lol d12

--- 2025/day12/script.js
+++ 2025/day12/script.js
@@ -13,7 +13,14 @@ const parse = input => input.split('\n').map(line => {
 // when tested with what I think is a sufficient extra space, it still yields the same result, so I went for it as an answer..
 
 let shapeSizes = [7, 7, 7, 6, 7, 5];
+let shapeSizeFull = 9;
 
-const run = (data, extraSpaceRequired = 200) => data.filter(space => space.width * space.height - extraSpaceRequired >= space.counts.reduce((a, v, i) => a + v * shapeSizes[i], 0) ).length
+const run = (data) => data.filter((space, id) => {
+    let maxOccupied = space.counts.reduce((a, v, i) => a + v * shapeSizeFull, 0);
+    let minOccupied = space.counts.reduce((a, v, i) => a + v * shapeSizes[i], 0);    
+    let area = space.width * space.height;
+    if (area < maxOccupied && area >= minOccupied) console.log('Undetermined shape, naive method failed', id);
+    return area >= maxOccupied;
+}).length
 
 console.log('p1', run(parse(input)));
